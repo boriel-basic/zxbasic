c2c556b1a426f931643cfb8d3cf8eae2fa644c1a
leirob <leirob@localhost>
2010-11-19 00:27:48 +0000

    Moved to trunk

---
+++ scratch/fixed.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# A program that displays fixed point numbers in hexa
+
+import sys
+
+number = float(sys.argv[1])
+number1 = 0xFFFFFFFF & int(number * 2**16)
+DE = number1 >> 16
+HL = number1 & 0xFFFF
+
+print "%f = %04X : %04X" % (number, DE, HL)
+
+
+
