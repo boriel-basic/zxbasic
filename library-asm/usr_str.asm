81f117e02b3333c9c47b1ad9f4f86a1b1e49a8bc
leirob <leirob@localhost>
2010-11-29 10:50:19 +0000

    USR now handles all string calls (STR value normalization)

--- library-asm/usr_str.asm
+++ library-asm/usr_str.asm
@@ -2,10 +2,16 @@
 ; If the str is EMPTY or not a letter, 0 is returned and ERR_NR set
 ; to "A: Invalid Argument"
 
+; On entry HL points to the string
+; and A register is non-zero if the string must be freed (TMP string)
+
 #include once <error.asm>
 #include once <const.asm>
+#include once <free.asm>
 
 USR_STR:
+    ld d, a     ; Saves A flag
+
 	ld a, h
 	or l
 	jr z, USR_ERROR ; a$ = NULL => Invalid Arg
@@ -27,9 +33,22 @@ USR_STR:
 	add hl, hl	 ; hl = A * 8
 	ld bc, (UDG)
 	add hl, bc
+    
+    ;; Now checks if the string must be released
+    ld d, a
+    or a
+    ret z   ; return if not
+
+    push hl ; saves result since __MEM_FREE changes HL
+    call __MEM_FREE
+    pop hl
 	ret
 
 USR_ERROR:
+    ld a, d
+    or a
+    call nz, __MEM_FREE
+
 	ld a, ERROR_InvalidArg
 	ld (ERR_NR), a
 	ld hl, 0
