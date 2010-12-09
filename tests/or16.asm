f4a287a5c1f717a8210237a3a388f38208893f0c
leirob <leirob@localhost>
2010-12-09 23:15:28 +0000

    Test for 16 bit boolean OR

---
+++ tests/or16.asm
@@ -0,0 +1,63 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld hl, (_a)
+	ld a, h
+	or l
+	ld (_b), a
+	ld hl, (_a)
+	ld a, 0FFh
+	ld (_b), a
+	ld hl, (_a)
+	ld a, h
+	or l
+	ld (_b), a
+	ld hl, (_a)
+	ld a, 0FFh
+	ld (_b), a
+	ld hl, (_a)
+	push hl
+	ld hl, (_a)
+	ex de, hl
+	pop hl
+	ld a, h
+	or l
+	or d
+	or e
+	ld (_b), a
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	
+ZXBASIC_USER_DATA:
+_a:
+	DEFB 00, 00
+_b:
+	DEFB 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
