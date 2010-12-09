50f4ed9d14a401b832da2723736acb05ddf67751
Jose Rodriguez <boriel@users.noreply.github.com>
2010-12-09 23:45:09 +0000

    Fixed a comment

--- library-asm/bor16.asm
+++ library-asm/bor16.asm
@@ -1,6 +1,6 @@
 ; vim:ts=4:et:
 ; FASTCALL bitwise or 16 version.
-; result in Accumulator (0 False, not 0 True)
+; result in HL
 ; __FASTCALL__ version (operands: A, H)
 ; Performs 16bit or 16bit and returns the boolean
 ; Input: HL, DE
