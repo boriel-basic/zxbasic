c9b9b986ed78b473617c39b434a409a65e44a667
Jose Rodriguez <boriel@users.noreply.github.com>
2010-12-09 19:30:05 +0000

    Better test for 8 bits boolean AND

--- tests/and0_8bit.asm
+++ tests/and0_8bit.asm
@@ -12,9 +12,21 @@ __START_PROGRAM:
 	ei
 	ld a, (_a)
 	xor a
-	ld (_a), a
+	ld (_b), a
+	ld a, (_a)
+	ld (_b), a
+	ld a, (_a)
 	xor a
-	ld (_a), a
+	ld (_b), a
+	ld a, (_a)
+	ld (_b), a
+	ld a, (_a)
+	push af
+	ld a, (_a)
+	ld h, a
+	pop af
+	call __AND8
+	ld (_b), a
 	ld hl, 0
 	ld b, h
 	ld c, l
@@ -31,9 +43,24 @@ __END_PROGRAM:
 	ret
 __CALL_BACK__:
 	DEFW 0
+#line 1 "and8.asm"
+	; FASTCALL boolean and 8 version.
+	; result in Accumulator (0 False, not 0 True)
+; __FASTCALL__ version (operands: A, H)
+	; Performs 8bit and 8bit and returns the boolean
+	
+__AND8:
+		or a
+		ret z
+		ld a, h
+		ret 
+	
+#line 35 "and0_8bit.bas"
 	
 ZXBASIC_USER_DATA:
 _a:
+	DEFB 00
+_b:
 	DEFB 00
 	; Defines DATA END --> HEAP size is 0
 ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
