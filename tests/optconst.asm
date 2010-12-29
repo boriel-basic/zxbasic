724c4ebd7091a64230b1603c06b83680a2d89d71
leirob <leirob@localhost>
2010-12-29 23:03:30 +0000

    Test updated.

--- tests/optconst.asm
+++ tests/optconst.asm
@@ -470,9 +470,7 @@ BRIGHT_TMP:
 	; Sets OVER flag in P_FLAG permanently
 ; Parameter: OVER flag in bit 0 of A register
 #line 1 "copy_attr.asm"
-	
 #line 4 "/home/boriel/src/zxb/trunk/library-asm/copy_attr.asm"
-	
 	
 COPY_ATTR:
 		; Just copies current permanent attribs to temporal attribs
@@ -495,7 +493,6 @@ COPY_ATTR:
 	
 	
 __SET_ATTR_MODE:		; Another entry to set print modes. A contains (P_FLAG)
-	
 	
 		LOCAL TABLE	
 		LOCAL CONT2
@@ -529,8 +526,8 @@ TABLE:
 		and (hl)		; OVER 2 MODE
 		or  (hl)		; OVER 3 MODE 
 	
-#line 65 "/home/boriel/src/zxb/trunk/library-asm/copy_attr.asm"
 	
+#line 65 "/home/boriel/src/zxb/trunk/library-asm/copy_attr.asm"
 __REFRESH_TMP:
 		ld a, (hl)
 		and 10101010b
