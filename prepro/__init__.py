7c1f14f5b25e52fb2e2d5c4eb30c0beb573ee3ea
Jose Rodriguez <boriel@users.noreply.github.com>
2011-01-12 08:00:21 +0000

    Fixed some import bugs.

--- prepro/__init__.py
+++ prepro/__init__.py
@@ -3,8 +3,8 @@
 # vim: ts=4:sw=4:et:
 
 from exceptions import PreprocError
-from definestable import DefinesTable
 from id import ID
+from definestable import DefinesTable
 from macrocall import MacroCall
 
 
