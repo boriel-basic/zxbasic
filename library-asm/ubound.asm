fd6df84644af05983122a4455fd98bb6be150f0a
leirob <leirob@localhost>
2011-02-12 14:27:53 +0000

    Separated in two libraries

--- library-asm/ubound.asm
+++ library-asm/ubound.asm
@@ -6,27 +6,7 @@
 ; This code is released under the BSD License
 ; ---------------------------------------------------------
 
-; Implements the LBOUND(array, N) function
-
-; Parameters:
-;   HL = N (dimension)
-;   [stack - 2] -> LBound table for the var
-;   Returns entry [N] in HL
-
-__LBOUND:
-    add hl, hl      ; hl *= 2
-    ex de, hl
-    pop hl
-    ex (sp), hl     ; __CALLEE
-
-    add hl, de      ; hl += OFFSET __LBOUND._xxxx
-    ld e, (hl)      ; de = (hl)
-    inc hl
-    ld d, (hl)
-
-    ex de, hl       ; hl = de => returns result in HL
-    ret
-
+#include once <lbound.asm>
 
 __UBOUND:
     add hl, hl
