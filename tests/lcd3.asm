a36caf81d11623f5f4ec5318fb76558906209345
leirob <leirob@localhost>
2011-03-09 20:18:56 +0000

    Test updated

--- tests/lcd3.asm
+++ tests/lcd3.asm
@@ -1185,9 +1185,6 @@ __ATTR_ADDR:
 	    ; calc start address in DE (as (32 * d) + e)
     ; Contributed by Santiago Romero at http://www.speccy.org
 	    ld h, 0                     ;  7 T-States
-	    ;; ld l, d
-	    ;; add hl, hl   ; HL = HL*2
-	    ;; add hl, hl   ; HL = HL*4
 	    ld a, d                     ;  4 T-States
 	    add a, a     ; a * 2        ;  4 T-States
 	    add a, a     ; a * 4        ;  4 T-States
@@ -1855,6 +1852,128 @@ __STR_END:
 		ENDP
 	
 #line 154 "lcd3.bas"
+#line 1 "u32tofreg.asm"
+#line 1 "neg32.asm"
+__ABS32:
+		bit 7, d
+		ret z
+	
+__NEG32: ; Negates DEHL (Two's complement)
+		ld a, l
+		cpl
+		ld l, a
+	
+		ld a, h
+		cpl
+		ld h, a
+	
+		ld a, e
+		cpl
+		ld e, a
+		
+		ld a, d
+		cpl
+		ld d, a
+	
+		inc l
+		ret nz
+	
+		inc h
+		ret nz
+	
+		inc de
+		ret
+	
+#line 2 "u32tofreg.asm"
+__I8TOFREG:
+		ld l, a
+		rlca
+		sbc a, a	; A = SGN(A)
+		ld h, a
+		ld e, a
+		ld d, a
+	
+__I32TOFREG:	; Converts a 32bit signed integer (stored in DEHL)
+					; to a Floating Point Number returned in (A ED CB)
+	
+		ld a, d
+		or a		; Test sign
+	
+		jp p, __U32TOFREG	; It was positive, proceed as 32bit unsigned
+	
+		call __NEG32		; Convert it to positive
+		call __U32TOFREG	; Convert it to Floating point
+	
+		set 7, e			; Put the sign bit (negative) in the 31bit of mantissa
+		ret
+	
+__U8TOFREG:
+					; Converts an unsigned 8 bit (A) to Floating point
+		ld l, a
+		ld h, 0
+		ld e, h
+		ld d, h
+	
+__U32TOFREG:	; Converts an unsigned 32 bit integer (DEHL)
+					; to a Floating point number returned in A ED CB
+	
+	    PROC
+	
+	    LOCAL __U32TOFREG_END
+	
+		ld a, d
+		or e
+		or h
+		or l
+	    ld b, d
+		ld c, e		; Returns 00 0000 0000 if ZERO
+		ret z
+	
+		push de
+		push hl
+	
+		exx
+		pop de  ; Loads integer into B'C' D'E' 
+		pop bc
+		exx
+	
+		ld l, 128	; Exponent
+		ld bc, 0	; DEBC = 0
+		ld d, b
+		ld e, c
+	
+__U32TOFREG_LOOP: ; Also an entry point for __F16TOFREG
+		exx
+		ld a, d 	; B'C'D'E' == 0 ?
+		or e
+		or b
+		or c
+		jp z, __U32TOFREG_END	; We are done
+	
+		srl b ; Shift B'C' D'E' >> 1, output bit stays in Carry
+		rr c
+		rr d
+		rr e
+		exx
+	
+		rr e ; Shift EDCB >> 1, inserting the carry on the left
+		rr d
+		rr c
+		rr b
+	
+		inc l	; Increment exponent
+		jp __U32TOFREG_LOOP
+	
+	
+__U32TOFREG_END:
+		exx
+	    ld a, l     ; Puts the exponent in a
+		res 7, e	; Sets the sign bit to 0 (positive)
+	
+		ret
+	    ENDP
+	
+#line 155 "lcd3.bas"
 #line 1 "addf.asm"
 	
 	
@@ -1877,7 +1996,7 @@ __ADDF:	; Addition
 	
 		jp __FPSTACK_POP
 	
-#line 155 "lcd3.bas"
+#line 156 "lcd3.bas"
 #line 1 "storestr2.asm"
 	; Similar to __STORE_STR, but this one is called when
 	; the value of B$ if already duplicated onto the stack.
@@ -2097,7 +2216,7 @@ __STORE_STR2:
 	
 		ret
 	
-#line 156 "lcd3.bas"
+#line 157 "lcd3.bas"
 #line 1 "printstr.asm"
 	
 	
@@ -2155,41 +2274,10 @@ __PRINT_STR:
 	
 			ENDP
 	
-#line 157 "lcd3.bas"
+#line 158 "lcd3.bas"
 	
 #line 1 "ftou32reg.asm"
-#line 1 "neg32.asm"
-__ABS32:
-		bit 7, d
-		ret z
 	
-__NEG32: ; Negates DEHL (Two's complement)
-		ld a, l
-		cpl
-		ld l, a
-	
-		ld a, h
-		cpl
-		ld h, a
-	
-		ld a, e
-		cpl
-		ld e, a
-		
-		ld a, d
-		cpl
-		ld d, a
-	
-		inc l
-		ret nz
-	
-		inc h
-		ret nz
-	
-		inc de
-		ret
-	
-#line 2 "ftou32reg.asm"
 	
 __FTOU32REG:	; Converts a Float to (un)signed 32 bit integer (NOTE: It's ALWAYS 32 bit signed)
 					; Input FP number in A EDCB (A exponent, EDCB mantissa)
@@ -2265,8 +2353,7 @@ __FTOU8:	; Converts float in C ED LH to Unsigned byte in A
 		ld a, l
 		ret
 	
-#line 159 "lcd3.bas"
-	
+#line 160 "lcd3.bas"
 #line 1 "loadstr.asm"
 	
 	
@@ -2312,97 +2399,7 @@ __LOADSTR:		; __FASTCALL__ entry
 			pop hl	; Recovers destiny in hl as result
 			ret
 #line 161 "lcd3.bas"
-#line 1 "u32tofreg.asm"
 	
-__I8TOFREG:
-		ld l, a
-		rlca
-		sbc a, a	; A = SGN(A)
-		ld h, a
-		ld e, a
-		ld d, a
-	
-__I32TOFREG:	; Converts a 32bit signed integer (stored in DEHL)
-					; to a Floating Point Number returned in (A ED CB)
-	
-		ld a, d
-		or a		; Test sign
-	
-		jp p, __U32TOFREG	; It was positive, proceed as 32bit unsigned
-	
-		call __NEG32		; Convert it to positive
-		call __U32TOFREG	; Convert it to Floating point
-	
-		set 7, e			; Put the sign bit (negative) in the 31bit of mantissa
-		ret
-	
-__U8TOFREG:
-					; Converts an unsigned 8 bit (A) to Floating point
-		ld l, a
-		ld h, 0
-		ld e, h
-		ld d, h
-	
-__U32TOFREG:	; Converts an unsigned 32 bit integer (DEHL)
-					; to a Floating point number returned in A ED CB
-	
-	    PROC
-	
-	    LOCAL __U32TOFREG_END
-	
-		ld a, d
-		or e
-		or h
-		or l
-	    ld b, d
-		ld c, e		; Returns 00 0000 0000 if ZERO
-		ret z
-	
-		push de
-		push hl
-	
-		exx
-		pop de  ; Loads integer into B'C' D'E' 
-		pop bc
-		exx
-	
-		ld l, 128	; Exponent
-		ld bc, 0	; DEBC = 0
-		ld d, b
-		ld e, c
-	
-__U32TOFREG_LOOP: ; Also an entry point for __F16TOFREG
-		exx
-		ld a, d 	; B'C'D'E' == 0 ?
-		or e
-		or b
-		or c
-		jp z, __U32TOFREG_END	; We are done
-	
-		srl b ; Shift B'C' D'E' >> 1, output bit stays in Carry
-		rr c
-		rr d
-		rr e
-		exx
-	
-		rr e ; Shift EDCB >> 1, inserting the carry on the left
-		rr d
-		rr c
-		rr b
-	
-		inc l	; Increment exponent
-		jp __U32TOFREG_LOOP
-	
-	
-__U32TOFREG_END:
-		exx
-	    ld a, l     ; Puts the exponent in a
-		res 7, e	; Sets the sign bit to 0 (positive)
-	
-		ret
-	    ENDP
-	
-#line 162 "lcd3.bas"
 	
 ZXBASIC_USER_DATA:
 _adr:
