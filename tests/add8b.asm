16446068c0d4862b2b50822d288a7d5ea6b506e2
boriel <boriel@localhost>
2011-03-12 19:52:41 +0000

    Added ADD8b test
    An even more complex test for ADD8

---
+++ tests/add8b.asm
@@ -0,0 +1,51 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld hl, (_a - 1)
+	ld a, (_a)
+	add a, h
+	push af
+	ld a, (_a)
+	add a, 2
+	push af
+	ld hl, (_a - 1)
+	pop af
+	add a, h
+	ld h, a
+	pop af
+	add a, h
+	ld (_a), a
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	
+ZXBASIC_USER_DATA:
+_a:
+	DEFB 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
