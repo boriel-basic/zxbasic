716fc10142cdf767a9c3a7cf3b5a011f962b97af
boriel <boriel@localhost>
2011-03-12 14:49:51 +0000

    Added test for FOR..NEXT
    This new test checks for a bug in constant step values.

---
+++ tests/fornext3.asm
@@ -0,0 +1,52 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld hl, 0
+	ld (_i), hl
+	jp __LABEL0
+__LABEL4:
+__LABEL5:
+	ld hl, (_i)
+	inc hl
+	ld (_i), hl
+__LABEL0:
+	ld hl, 10000
+	ld de, (_i)
+	or a
+	sbc hl, de
+	jp nc, __LABEL4
+__LABEL3:
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	
+ZXBASIC_USER_DATA:
+_i:
+	DEFB 00, 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
