1f7cd746eaf77a83acc4411ced06a886fad203bb
boriel <boriel@localhost>
2011-03-13 20:10:39 +0000

    BugFix for array assignement.
    Fixes a bug for A(n) = Z, being Z 8bit type
    and n a variable.

--- backend/__array.py
+++ backend/__array.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# vim:ts=4:et:sw=4:
 
 # --------------------------------------------------------------
 # Copyleft (k) 2008, by Jose M. Rodriguez-Rosa
@@ -9,328 +10,349 @@
 # intermediate-code traductions
 # --------------------------------------------------------------
 
-from __common import REQUIRES, is_float
+from __common import REQUIRES, is_float, is_int
+from __8bit import _8bit_oper
 from __f16 import f16, _f16_oper
 from __float import _float, _fpop, _fpush, _float_oper
 
 def _addr(value):
-	''' Common subroutine for emmiting array address
-	'''
-	output = []
+    ''' Common subroutine for emmiting array address
+    '''
+    output = []
 
-	try:
-		indirect = False
-		if value[0] == '*':
-			indirect = True
-			value = value[1:]
+    try:
+        indirect = False
+        if value[0] == '*':
+            indirect = True
+            value = value[1:]
 
-		value = int(value) & 0xFFFF
-		if indirect:
-			output.append('ld hl, (%s)' % str(value))
-		else:
-			output.append('ld hl, %s' % str(value))
+        value = int(value) & 0xFFFF
+        if indirect:
+            output.append('ld hl, (%s)' % str(value))
+        else:
+            output.append('ld hl, %s' % str(value))
 
-	except ValueError:
-		if value[0] == '_':
-			output.append('ld hl, %s' % str(value))
-			if indirect:
-				output.append('ld c, (hl)')
-				output.append('inc hl')
-				output.append('ld h, (hl)')
-				output.append('ld l, c')
-		else:
-			output.append('pop hl')
-			if indirect:
-				output.append('ld c, (hl)')
-				output.append('inc hl')
-				output.append('ld h, (hl)')
-				output.append('ld l, c')
+    except ValueError:
+        if value[0] == '_':
+            output.append('ld hl, %s' % str(value))
+            if indirect:
+                output.append('ld c, (hl)')
+                output.append('inc hl')
+                output.append('ld h, (hl)')
+                output.append('ld l, c')
+        else:
+            output.append('pop hl')
+            if indirect:
+                output.append('ld c, (hl)')
+                output.append('inc hl')
+                output.append('ld h, (hl)')
+                output.append('ld l, c')
 
-	output.append('call __ARRAY')
-	REQUIRES.add('array.asm')
-	
-	return output
+    output.append('call __ARRAY')
+    REQUIRES.add('array.asm')
+    
+    return output
 
 
 
 def _aaddr(ins):
-	''' Loads the address of an array element
-	into the stack.
-	'''
-	output = _addr(ins.quad[2])
-	output.append('push hl')
+    ''' Loads the address of an array element
+    into the stack.
+    '''
+    output = _addr(ins.quad[2])
+    output.append('push hl')
 
-	return output
+    return output
 
 
 def _aload8(ins):
-	''' Loads an 8 bit value from a memory address
-	If 2nd arg. start with '*', it is always treated as
-	an indirect value.
-	'''
-	output = _addr(ins.quad[2])
-	output.append('ld a, (hl)')
-	output.append('push af')
+    ''' Loads an 8 bit value from a memory address
+    If 2nd arg. start with '*', it is always treated as
+    an indirect value.
+    '''
+    output = _addr(ins.quad[2])
+    output.append('ld a, (hl)')
+    output.append('push af')
 
-	return output
+    return output
 
 
 
 def _aload16(ins):
-	''' Loads a 16 bit value from a memory address
-	If 2nd arg. start with '*', it is always treated as
-	an indirect value.
-	'''
-	output = _addr(ins.quad[2])
-	
-	output.append('ld e, (hl)')
-	output.append('inc hl')
-	output.append('ld d, (hl)')
-	output.append('ex de, hl')
-	output.append('push hl')
+    ''' Loads a 16 bit value from a memory address
+    If 2nd arg. start with '*', it is always treated as
+    an indirect value.
+    '''
+    output = _addr(ins.quad[2])
+    
+    output.append('ld e, (hl)')
+    output.append('inc hl')
+    output.append('ld d, (hl)')
+    output.append('ex de, hl')
+    output.append('push hl')
 
-	return output
+    return output
 
 
 def _aload32(ins):
-	''' Load a 32 bit value from a memory address
-	If 2nd arg. start with '*', it is always treated as
-	an indirect value.
-	'''
-	output = _addr(ins.quad[2])
+    ''' Load a 32 bit value from a memory address
+    If 2nd arg. start with '*', it is always treated as
+    an indirect value.
+    '''
+    output = _addr(ins.quad[2])
 
-	output.append('call __ILOAD32')
-	output.append('push de')
-	output.append('push hl')
+    output.append('call __ILOAD32')
+    output.append('push de')
+    output.append('push hl')
 
-	REQUIRES.add('iload32.asm')
+    REQUIRES.add('iload32.asm')
 
-	return output
+    return output
 
 
 
 def _aloadf(ins):
-	''' Loads a floating point value from a memory address.
-	If 2nd arg. start with '*', it is always treated as
-	an indirect value.
-	'''
-	output = _addr(ins.quad[2])
-	output.append('call __LOADF')
-	output.extend(_fpush())
+    ''' Loads a floating point value from a memory address.
+    If 2nd arg. start with '*', it is always treated as
+    an indirect value.
+    '''
+    output = _addr(ins.quad[2])
+    output.append('call __LOADF')
+    output.extend(_fpush())
 
-	REQUIRES.add('iloadf.asm')
+    REQUIRES.add('iloadf.asm')
 
-	return output
+    return output
 
 
 
 def _aloadstr(ins):
-	''' Loads a string value from a memory address.
-	'''
-	output = _addr(ins.quad[2])
+    ''' Loads a string value from a memory address.
+    '''
+    output = _addr(ins.quad[2])
 
-	output.append('call __ILOADSTR')
-	output.append('push hl')
-	REQUIRES.add('loadstr.asm')
+    output.append('call __ILOADSTR')
+    output.append('push hl')
+    REQUIRES.add('loadstr.asm')
 
-	return output
-		
+    return output
+        
 
 
 def _astore8(ins):
-	''' Stores 2º operand content into address of 1st operand.
-	1st operand is an array element. Dimensions are pushed into the 
-	stack.
-	Use '*' for indirect store on 1st operand (A pointer to an array)
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores 2º operand content into address of 1st operand.
+    1st operand is an array element. Dimensions are pushed into the 
+    stack.
+    Use '*' for indirect store on 1st operand (A pointer to an array)
+    '''
+    output = _addr(ins.quad[1])
+    op = ins.quad[2]
 
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    indirect = op[0] == '*'
+    if indirect:
+        op = op[1:]
 
-	try:
-		value = int(ins.quad[2]) & 0xFFFF
-		if indirect:
-			output.append('ld a, (%i)' % value)
-			output.append('ld (hl), a')
-		else:
-			value &= 0xFF
-			output.append('ld (hl), %i' % value)
-	except ValueError:
-		output.append('pop af')
-		output.append('ld (hl), a')
+    immediate = op[0] == '#'
+    if immediate:
+        op = op[1:]
 
-	return output
+    if is_int(op):
+        if indirect:
+            if immediate:
+                op = str(int(op) & 0xFFFF) # Truncate to 16bit pointer
+                output.append('ld a, (%s)' % op)
+            else:
+                output.append('ld de, (%s)' % op)
+                output.append('ld a, (de)')
+        else:
+            op = str(int(op) & 0xFF) # Truncate to byte
+            output.append('ld (hl), %s' % op)
+            return output
+
+    elif op[0] == '_':
+        if indirect:
+            if immediate:
+                output.append('ld a, (%s)' % op) # Redundant case: *#_id == _id
+            else:
+                output.append('ld de, (%s)' % op) # *_id
+                output.append('ld a, (de)')
+        else:
+            if immediate:
+                output.append('ld a, %s' % op) # #_id
+            else:
+                output.append('ld a, (%s)' % op) # _id
+    else:        
+        output.append('pop af') # tn
+
+    output.append('ld (hl), a')
+
+    return output
 
 
 
 def _astore16(ins):
-	''' Stores 2º operand content into address of 1st operand.
-	store16 a, x =>  *(&a) = x
-	Use '*' for indirect store on 1st operand.
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores 2º operand content into address of 1st operand.
+    store16 a, x =>  *(&a) = x
+    Use '*' for indirect store on 1st operand.
+    '''
+    output = _addr(ins.quad[1])
 
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    value = ins.quad[2]
+    if value[0] == '*':
+        value = value[1:]
+        indirect = True
+    else:
+        indirect = False
 
-	try:
-		value = int(ins.quad[2]) & 0xFFFF
-		output.append('ld de, %i' % value)
-		if indirect:
-			output.append('call __LOAD_DE_DE')
-			REQUIRES.add('lddede.asm')
+    try:
+        value = int(ins.quad[2]) & 0xFFFF
+        output.append('ld de, %i' % value)
+        if indirect:
+            output.append('call __LOAD_DE_DE')
+            REQUIRES.add('lddede.asm')
 
-	except ValueError:
-		output.append('pop de')
+    except ValueError:
+        output.append('pop de')
 
-	output.append('ld (hl), e')
-	output.append('inc hl')
-	output.append('ld (hl), d')
+    output.append('ld (hl), e')
+    output.append('inc hl')
+    output.append('ld (hl), d')
 
-	return output
+    return output
 
 
 
 def _astore32(ins):
-	''' Stores 2º operand content into address of 1st operand.
-	store16 a, x =>  *(&a) = x
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores 2º operand content into address of 1st operand.
+    store16 a, x =>  *(&a) = x
+    '''
+    output = _addr(ins.quad[1])
 
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    value = ins.quad[2]
+    if value[0] == '*':
+        value = value[1:]
+        indirect = True
+    else:
+        indirect = False
 
-	try:
-		value = int(ins.quad[2]) & 0xFFFFFFFF # Immediate?
-		if indirect:
-			output.append('push hl')
-			output.append('ld hl, %i' % (value & 0xFFFF))
-			output.append('call __ILOAD32')
-			output.append('ld b, h')
-			output.append('ld c, l') # BC = Lower 16 bits
-			output.append('pop hl')
-			REQUIRES.add('iload32.asm')
-		else:
-			output.append('ld de, %i' % (value >> 16))
-			output.append('ld bc, %i' % (value & 0xFFFF))
-	except ValueError:
-		output.append('pop bc')
-		output.append('pop de')
+    try:
+        value = int(ins.quad[2]) & 0xFFFFFFFF # Immediate?
+        if indirect:
+            output.append('push hl')
+            output.append('ld hl, %i' % (value & 0xFFFF))
+            output.append('call __ILOAD32')
+            output.append('ld b, h')
+            output.append('ld c, l') # BC = Lower 16 bits
+            output.append('pop hl')
+            REQUIRES.add('iload32.asm')
+        else:
+            output.append('ld de, %i' % (value >> 16))
+            output.append('ld bc, %i' % (value & 0xFFFF))
+    except ValueError:
+        output.append('pop bc')
+        output.append('pop de')
 
-	output.append('call __STORE32')
-	REQUIRES.add('store32.asm')
+    output.append('call __STORE32')
+    REQUIRES.add('store32.asm')
 
-	return output
+    return output
 
 
 
 def _astoref16(ins):
-	''' Stores 2º operand content into address of 1st operand.
-	storef16 a, x =>  *(&a) = x
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores 2º operand content into address of 1st operand.
+    storef16 a, x =>  *(&a) = x
+    '''
+    output = _addr(ins.quad[1])
 
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    value = ins.quad[2]
+    if value[0] == '*':
+        value = value[1:]
+        indirect = True
+    else:
+        indirect = False
 
-	if indirect:
-		output.append('push hl')
-		output.extend(_f16_oper(ins.quad[2], useBC = True))
-		output.append('pop hl')
-		REQUIRES.add('iload32.asm')
-	else:
-		output.extend(_f16_oper(ins.quad[2], useBC = True))
+    if indirect:
+        output.append('push hl')
+        output.extend(_f16_oper(ins.quad[2], useBC = True))
+        output.append('pop hl')
+        REQUIRES.add('iload32.asm')
+    else:
+        output.extend(_f16_oper(ins.quad[2], useBC = True))
 
-	output.append('call __STORE32')
-	REQUIRES.add('store32.asm')
+    output.append('call __STORE32')
+    REQUIRES.add('store32.asm')
 
-	return output
+    return output
 
 
 
 def _astoref(ins):
-	''' Stores a floating point value into a memory address.
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores a floating point value into a memory address.
+    '''
+    output = _addr(ins.quad[1])
 
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    value = ins.quad[2]
+    if value[0] == '*':
+        value = value[1:]
+        indirect = True
+    else:
+        indirect = False
 
-	if indirect:
-		output.append('push hl')
-		output.extend(_float_oper(ins.quad[2]))
-		output.append('pop hl')
-	else:
-		output.extend(_float_oper(ins.quad[2]))
+    if indirect:
+        output.append('push hl')
+        output.extend(_float_oper(ins.quad[2]))
+        output.append('pop hl')
+    else:
+        output.extend(_float_oper(ins.quad[2]))
 
-	output.append('call __STOREF')
-	REQUIRES.add('storef.asm')
+    output.append('call __STOREF')
+    REQUIRES.add('storef.asm')
 
-	return output
+    return output
 
 
 
 def _astorestr(ins):
-	''' Stores a string value into a memory address.
-	It copies content of 2nd operand (string), into 1st, reallocating
-	dynamic memory for the 1st str. These instruction DOES ALLOW
-	inmediate strings for the 2nd parameter, starting with '#'.
-	'''
-	output = _addr(ins.quad[1])
+    ''' Stores a string value into a memory address.
+    It copies content of 2nd operand (string), into 1st, reallocating
+    dynamic memory for the 1st str. These instruction DOES ALLOW
+    inmediate strings for the 2nd parameter, starting with '#'.
+    '''
+    output = _addr(ins.quad[1])
 
-	temporal = False
-	value = ins.quad[2]
-	if value[0] == '*':
-		value = value[1:]
-		indirect = True
-	else:
-		indirect = False
+    temporal = False
+    value = ins.quad[2]
+    if value[0] == '*':
+        value = value[1:]
+        indirect = True
+    else:
+        indirect = False
 
-	if value[0] == '_':
-		output.append('ld de, (%s)' % value)
+    if value[0] == '_':
+        output.append('ld de, (%s)' % value)
 
-		if indirect:
-			output.append('call __LOAD_DE_DE')
-			REQUIRES.add('lddede.asm')
+        if indirect:
+            output.append('call __LOAD_DE_DE')
+            REQUIRES.add('lddede.asm')
 
-	elif value[0] == '#':
-		output.append('ld de, %s' % value[1:])
-	else:
-		output.append('pop de')
-		temporal = True
-		if indirect:
-			output.append('call __LOAD_DE_DE')
-			REQUIRES.add('lddede.asm')
+    elif value[0] == '#':
+        output.append('ld de, %s' % value[1:])
+    else:
+        output.append('pop de')
+        temporal = True
+        if indirect:
+            output.append('call __LOAD_DE_DE')
+            REQUIRES.add('lddede.asm')
 
-	if not temporal:
-		output.append('call __STORE_STR')
-		REQUIRES.add('storestr.asm')
-	else: # A value already on dynamic memory
-		output.append('call __STORE_STR2')
-		REQUIRES.add('storestr2.asm')
+    if not temporal:
+        output.append('call __STORE_STR')
+        REQUIRES.add('storestr.asm')
+    else: # A value already on dynamic memory
+        output.append('call __STORE_STR2')
+        REQUIRES.add('storestr2.asm')
 
-	return output
+    return output
 
 
 
