c6c027e2a18f1dd73d3472377f8525d835c9d562
Jose Rodriguez <boriel@users.noreply.github.com>
2011-03-13 01:01:18 +0000

    Test updated: str0

--- tests/str0.asm
+++ tests/str0.asm
@@ -2018,51 +2018,6 @@ __PRINT_STR:
 	
 #line 62 "str0.bas"
 	
-#line 1 "loadstr.asm"
-	
-	
-	; Loads a string (ptr) from HL
-	; and duplicates it on dynamic memory again
-	; Finally, it returns result pointer in HL
-	
-__ILOADSTR:		; This is the indirect pointer entry HL = (HL)
-			ld a, h
-			or l
-			ret z
-			ld a, (hl)
-			inc hl
-			ld h, (hl)
-			ld l, a
-	
-__LOADSTR:		; __FASTCALL__ entry
-			ld a, h
-			or l
-			ret z	; Return if NULL
-	
-			ld c, (hl)
-			inc hl
-			ld b, (hl)
-			dec hl  ; BC = LEN(a$)
-	
-			inc bc
-			inc bc	; BC = LEN(a$) + 2 (two bytes for length)
-	
-			push hl
-			push bc
-			call __MEM_ALLOC
-			pop bc  ; Recover length
-			pop de  ; Recover origin
-	
-			ld a, h
-			or l
-			ret z	; Return if NULL (No memory)
-	
-			ex de, hl ; ldir takes HL as source, DE as destiny, so SWAP HL,DE
-			push de	; Saves destiny start
-			ldir	; Copies string (length number included)
-			pop hl	; Recovers destiny in hl as result
-			ret
-#line 64 "str0.bas"
 #line 1 "u32tofreg.asm"
 #line 1 "neg32.asm"
 __ABS32:
@@ -2184,7 +2139,7 @@ __U32TOFREG_END:
 		ret
 	    ENDP
 	
-#line 65 "str0.bas"
+#line 64 "str0.bas"
 	
 ZXBASIC_USER_DATA:
 _a:
