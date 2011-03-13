d430ba95aaa5602af5e33cc5a226091108751336
Jose Rodriguez <boriel@users.noreply.github.com>
2011-03-13 12:59:06 +0000

    Test updated: param1

--- tests/param1.asm
+++ tests/param1.asm
@@ -2010,51 +2010,6 @@ __STORE_STR:
 		pop hl		; Returns ptr to b$ in HL (Caller might needed to free it from memory)
 		ret
 #line 49 "param1.bas"
-#line 1 "loadstr.asm"
-	
-	
-	; Loads a string (ptr) from HL
-	; and duplicates it on dynamic memory again
-	; Finally, it returns result pointer in HL
-	
-__ILOADSTR:		; This is the indirect pointer entry HL = (HL)
-			ld a, h
-			or l
-			ret z
-			ld a, (hl)
-			inc hl
-			ld h, (hl)
-			ld l, a
-	
-__LOADSTR:		; __FASTCALL__ entry
-			ld a, h
-			or l
-			ret z	; Return if NULL
-	
-			ld c, (hl)
-			inc hl
-			ld b, (hl)
-			dec hl  ; BC = LEN(a$)
-	
-			inc bc
-			inc bc	; BC = LEN(a$) + 2 (two bytes for length)
-	
-			push hl
-			push bc
-			call __MEM_ALLOC
-			pop bc  ; Recover length
-			pop de  ; Recover origin
-	
-			ld a, h
-			or l
-			ret z	; Return if NULL (No memory)
-	
-			ex de, hl ; ldir takes HL as source, DE as destiny, so SWAP HL,DE
-			push de	; Saves destiny start
-			ldir	; Copies string (length number included)
-			pop hl	; Recovers destiny in hl as result
-			ret
-#line 50 "param1.bas"
 #line 1 "print_eol_attr.asm"
 	; Calls PRINT_EOL and then COPY_ATTR, so saves
 	; 3 bytes
@@ -2065,7 +2020,7 @@ __LOADSTR:		; __FASTCALL__ entry
 PRINT_EOL_ATTR:
 		call PRINT_EOL
 		jp COPY_ATTR
-#line 51 "param1.bas"
+#line 50 "param1.bas"
 	
 ZXBASIC_USER_DATA:
 _a:
