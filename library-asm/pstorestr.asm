a031155eef788c7d681c58f5a5a7699ee0810e5b
Jose Rodriguez <boriel@users.noreply.github.com>
2011-03-20 13:52:16 +0000

    Added module: pstorestr2.asm Like pstorestr, but for temporary strings

--- library-asm/pstorestr.asm
+++ library-asm/pstorestr.asm
@@ -1,7 +1,7 @@
 ; vim:ts=4:et:sw=4
 ; 
 ; Stores an string (pointer to the HEAP by DE) into the address pointed
-; by IX + BC
+; by (IX + BC). A new copy of the string is created into the HEAP
 ;
 
 #include once <storestr.asm>
