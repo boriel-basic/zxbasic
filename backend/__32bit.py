6fd0ab6aba628e2aa00bcf8d66d0eb0dca486585
boriel <boriel@localhost>
2011-08-20 23:46:13 +0100

    Fixed a bug in SUB32 which was not swapping opperands sometimes.

--- backend/__32bit.py
+++ backend/__32bit.py
@@ -246,7 +246,9 @@ def _sub32(ins):
             output.append('push hl')
             return output
 
-    output = _32bit_oper(op1, op2)
+    rev = op1[0] != 't' and op2[0] == 't'
+
+    output = _32bit_oper(op1, op2, rev)
     output.append('call __SUB32')
     output.append('push de')
     output.append('push hl')
