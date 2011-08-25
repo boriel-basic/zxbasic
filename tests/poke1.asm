6e161ba5adb29335b822c31540ae128f624660f8
Jose Rodriguez <boriel@users.noreply.github.com>
2011-08-25 21:27:03 +0100

    Added test for POKE Uinteger

--- tests/poke1.asm
+++ tests/poke1.asm
@@ -10,29 +10,7 @@ __START_PROGRAM:
 	add hl, sp
 	ld (__CALL_BACK__), hl
 	ei
-	xor a
-	ld (_j), a
-	jp __LABEL0
-__LABEL4:
-	ld hl, 65535
-	ex de, hl
-	ld hl, (_i)
-	ld (hl), e
-	inc hl
-	ld (hl), d
-	ld hl, (_i)
-	inc hl
-	ld (_i), hl
-__LABEL5:
-	ld a, (_j)
-	inc a
-	ld (_j), a
-__LABEL0:
-	ld a, 250
-	ld hl, (_j - 1)
-	cp h
-	jp nc, __LABEL4
-__LABEL3:
+	call _test
 	ld hl, 0
 	ld b, h
 	ld c, l
@@ -49,13 +27,60 @@ __END_PROGRAM:
 	ret
 __CALL_BACK__:
 	DEFW 0
+_test:
+	push ix
+	ld ix, 0
+	add ix, sp
+	ld hl, 0
+	push hl
+	push hl
+	inc sp
+	push ix
+	pop hl
+	ld bc, -3
+	add hl, bc
+	ex de, hl
+	ld hl, __LABEL6
+	ld bc, 2
+	ldir
+	ld (ix-1), 0
+	jp __LABEL0
+__LABEL4:
+	ld l, (ix-3)
+	ld h, (ix-2)
+	push hl
+	ld hl, 65535
+	ex de, hl
+	pop hl
+	ld (hl), e
+	inc hl
+	ld (hl), d
+	ld l, (ix-3)
+	ld h, (ix-2)
+	inc hl
+	ld (ix-3), l
+	ld (ix-2), h
+__LABEL5:
+	ld a, (ix-1)
+	inc a
+	ld (ix-1), a
+__LABEL0:
+	ld a, (ix-1)
+	push af
+	ld a, 250
+	pop hl
+	cp h
+	jp nc, __LABEL4
+__LABEL3:
+_test__leave:
+	ld sp, ix
+	pop ix
+	ret
 	
 ZXBASIC_USER_DATA:
-_i:
+__LABEL6:
 	DEFB 00h
 	DEFB 40h
-_j:
-	DEFB 00
 	; Defines DATA END --> HEAP size is 0
 ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
 	; Defines USER DATA Length in bytes
