ee6232eca5e7beb340c0c7625f8278300f8b5dbd
boriel <boriel@localhost>
2011-08-25 21:23:57 +0100

    Added test for POKE Uinteger

---
+++ tests/poke1.asm
@@ -0,0 +1,63 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	xor a
+	ld (_j), a
+	jp __LABEL0
+__LABEL4:
+	ld hl, 65535
+	ex de, hl
+	ld hl, (_i)
+	ld (hl), e
+	inc hl
+	ld (hl), d
+	ld hl, (_i)
+	inc hl
+	ld (_i), hl
+__LABEL5:
+	ld a, (_j)
+	inc a
+	ld (_j), a
+__LABEL0:
+	ld a, 250
+	ld hl, (_j - 1)
+	cp h
+	jp nc, __LABEL4
+__LABEL3:
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	
+ZXBASIC_USER_DATA:
+_i:
+	DEFB 00h
+	DEFB 40h
+_j:
+	DEFB 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
---
+++ tests/poke1.bas
@@ -0,0 +1,8 @@
+DIM i as Uinteger = 16384
+
+DIM j as Ubyte
+FOR j = 0 to 250
+    POKE Uinteger i, 65535
+    i = i + 1
+NEXT j
+
