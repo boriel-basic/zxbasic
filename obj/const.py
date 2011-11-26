4b57fa3c501be0016dd37cea971183fd3edb49eb
Jose Rodriguez <boriel@users.noreply.github.com>
2011-11-26 15:44:57 +0000

    Added SUFFIX-->Types table

--- obj/const.py
+++ obj/const.py
@@ -31,4 +31,6 @@ NAME_TYPES = dict([(TYPE_NAMES[x], x) for x in TYPE_NAMES.keys()]) # The reverse
 TYPE_SIZES = {'i8': 1, 'u8': 1, 'i16': 2, 'u16': 2, 'i32': 4, 'u32': 4,
              'fixed': 4, 'float': 5, 'string': 2, None: 0 }
 
+# Maps suffix to types
+SUFFIX_TYPE = {'$': 'string', '%': 'i16', '&': 'i32'}
 
