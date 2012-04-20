350bde03aaaeee30498ee3914f565a8d9650ef85
Jose Rodriguez <boriel@users.noreply.github.com>
2012-04-20 23:06:49 +0100

    Added library: LTI8 and LTE8 Implements signed comparison for 8 bits signed integers

---
+++ library-asm/lti8.asm
@@ -0,0 +1,21 @@
+
+__LTI8: ; Test 8 bit values A < H
+        ; Returns result in A: 0 = False, !0 = True
+        sub h
+
+__LTI:  ; Signed CMP
+        PROC
+        LOCAL __PE
+
+        ld a, 0  ; Sets default to false
+        jp pe, __PE
+        ; Overflow flag NOT set
+        ret p
+        dec a ; TRUE
+
+__PE:   ; Overflow set
+        ret m
+        dec a ; TRUE
+        ret
+        
+        ENDP
