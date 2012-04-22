d7469a805695598f6aa9b8df27f58b1befb624b5
Jose Rodriguez <boriel@users.noreply.github.com>
2012-04-22 01:16:17 +0100

    Bug Fix: GEI32 Fixed a bug for (Long) >= (Long) operation

--- backend/__32bit.py
+++ backend/__32bit.py
@@ -546,12 +546,11 @@ def _gei32(ins):
     op1, op2 = tuple(ins.quad[2:])
     rev = op1[0] != 't' and not is_int(op1) and op2[0] == 't'
     output = _32bit_oper(op1, op2, rev)
-    output.append('call __SUB32')
-    output.append('rl d')       # Move bit 31 to carry
-    output.append('ccf')        # Negates result => Carry if A >= B
-    output.append('sbc a, a')
+    output.append('call __LTI32')  # A = (a < b)
+    output.append('sub 1')         # Carry if !(a < b)
+    output.append('sbc a, a')      # A = !(a < b) = (a >= b)
     output.append('push af')
-    REQUIRES.add('sub32.asm')
+    REQUIRES.add('lti32.asm')
     return output
 
 
