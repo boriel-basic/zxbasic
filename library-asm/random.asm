7f3f8b88b0627f58d486eb7ee09f5e6d4a13d62e
boriel <boriel@localhost>
2012-05-30 16:45:28 +0100

    Enhaced RND Fixed a little bug in RND and replaced the RND generator with a much faster one from: http://www.worldofspectrum.org/forums/showthread.php?p=630575 (Marsaglia's Generator) Thanks to Britlion.

--- library-asm/random.asm
+++ library-asm/random.asm
@@ -31,77 +31,79 @@ TAKE_FRAMES:
 FRAMES EQU	23672
 	ENDP
 
-RANDOM_SEED_HIGH:	; RANDOM seed, 16 higher bits
-	DEFW 0000h		; HIGH RANDOM SEED
-
+RANDOM_SEED_HIGH EQU RAND+6	; RANDOM seed, 16 higher bits
 RANDOM_SEED_LOW 	EQU 23670	; RANDOM seed, 16 lower bits
 
 
 RAND:
-	; Returns a 32 bit random integer based on seed.
-	; This is a very fast and interesting generator
-	; See Numerical Recipes, 2nd ED
-
-	ld de, 25
-	ld hl, 26125
-	push de
-	push hl		; PUSH 32bit value 1664525
-
-	ld de, (RANDOM_SEED_HIGH)
-	ld hl, (RANDOM_SEED_LOW)
-	call __MUL32	; DE HL = 166425 * SEED
-
-	ld bc, 62303	; DEHL = 1013904223 = 15470 * 65536 + 62303
-	add hl, bc
-	ld (RANDOM_SEED_LOW), hl
-
-	ex de, hl
-	ld bc, 15470
-	adc hl, bc
-	ld (RANDOM_SEED_HIGH), hl
-	ex de, hl
-	ret
+    PROC
+    LOCAL RAND_LOOP
+    ld b, 2 ; Gives better results if run twice
+RAND_LOOP:
+    ld  hl,(RANDOM_SEED_LOW)   ; xz -> yw
+    ld  de,0C0DEh   ; yw -> zt
+    ld  (RANDOM_SEED_LOW),de  ; x = y, z = w
+    ld  a,e         ; w = w ^ ( w << 3 )
+    add a,a
+    add a,a
+    add a,a
+    xor e
+    ld  e,a
+    ld  a,h         ; t = x ^ (x << 1)
+    add a,a
+    xor h
+    ld  d,a
+    rra             ; t = t ^ (t >> 1) ^ w
+    xor d
+    xor e
+    ld  h,l         ; y = z
+    ld  l,a         ; w = t
+    ld  (RANDOM_SEED_HIGH),hl
+    djnz RAND_LOOP
+    ret
+    ENDP
 
 RND:
 	; Returns a FLOATING point integer
 	; using RAND as a mantissa
 	PROC
 	LOCAL RND_LOOP
-	LOCAL RND_END
 
 	call RAND	
-	ld a, d	
-	or e
-	or h
-	or l
     ld b, h
-	ld c, l
-	ret z		; Return 0 if 0
+    ld c, l
+
+    ld a, e
+    or d
+    or c
+    or b
+    ret z   ; Returns 0 if HL=DE=0
 
 	; We already have a random 32 bit mantissa in ED CB
+    ; From 0001h to FFFFh
+
 	ld l, 81h	; Exponent
-	set 7, e	; Recover highest mantissa bit
-	; At this point we have 1 .. 2) FP number;
-	; We subtract 1 to obtain the 0..1) Range
+	; At this point we have [0 .. 1) FP number;
 
-	; To subtract 1, we only have to subtract the highest byte (40h)
-	ld a, e
-	sub	40h
-	ld e, a
-
-	; Now we must shift left until highest bit is 1
+	; Now we must shift mantissa left until highest bit goes into carry
+    ld a, e ; Use a register for the higher byte for faster RR
 RND_LOOP:
 	dec l
 	sla b
 	rl c
 	rl d
 	rla
-	jp p, RND_LOOP	; Loop if bit 7 is not one
+	jp nc, RND_LOOP
 
-RND_END:
-	and 7Fh		; Clear highest bit (positive sign)
-	ld e, a
-    ld a, l     ; saves exponent in A
+    ; Now undo last mantissa left-shif
+    ccf ; Clears flag (it was set) to insert a 0 bit -> positive number
+    rra
+    rr d   
+    rr c
+    rr b
+    
+	ld e, a     ; E must have the highest byte
+    ld a, l     ; exponent in A
 	ret
 
 	ENDP
