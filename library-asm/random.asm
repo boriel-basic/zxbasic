6341987dc1644f3fd2c8cd1e886bab935cb1362f
boriel <boriel@localhost>
2012-05-30 16:52:45 +0100

    Code clean. Tabs removed. Comments enhanced.

--- library-asm/random.asm
+++ library-asm/random.asm
@@ -3,36 +3,36 @@
 #include once <mul32.asm>
 
 RANDOMIZE:
-	; Randomize with 32 bit seed in DE HL
-	; if SEED = 0, calls ROM to take frames as seed
-	PROC
+    ; Randomize with 32 bit seed in DE HL
+    ; if SEED = 0, calls ROM to take frames as seed
+    PROC
 
-	LOCAL TAKE_FRAMES
-	LOCAL FRAMES
-	
-	ld a, h
-	or l
-	or d
-	or e
-	jr z, TAKE_FRAMES
+    LOCAL TAKE_FRAMES
+    LOCAL FRAMES
+    
+    ld a, h
+    or l
+    or d
+    or e
+    jr z, TAKE_FRAMES
 
-	ld (RANDOM_SEED_LOW), hl
-	ld (RANDOM_SEED_HIGH), de
-	ret
+    ld (RANDOM_SEED_LOW), hl
+    ld (RANDOM_SEED_HIGH), de
+    ret
 
 TAKE_FRAMES:
-	; Takes the seed from frames
-	ld hl, (FRAMES)
-	ld (RANDOM_SEED_LOW), hl
-	ld hl, (FRAMES + 2)
-	ld (RANDOM_SEED_HIGH), hl
-	ret
+    ; Takes the seed from frames
+    ld hl, (FRAMES)
+    ld (RANDOM_SEED_LOW), hl
+    ld hl, (FRAMES + 2)
+    ld (RANDOM_SEED_HIGH), hl
+    ret
 
-FRAMES EQU	23672
-	ENDP
+FRAMES EQU    23672
+    ENDP
 
-RANDOM_SEED_HIGH EQU RAND+6	; RANDOM seed, 16 higher bits
-RANDOM_SEED_LOW 	EQU 23670	; RANDOM seed, 16 lower bits
+RANDOM_SEED_HIGH EQU RAND+6    ; RANDOM seed, 16 higher bits
+RANDOM_SEED_LOW     EQU 23670  ; RANDOM seed, 16 lower bits
 
 
 RAND:
@@ -64,12 +64,13 @@ RAND_LOOP:
     ENDP
 
 RND:
-	; Returns a FLOATING point integer
-	; using RAND as a mantissa
-	PROC
-	LOCAL RND_LOOP
+    ; Returns a FLOATING point integer
+    ; using RAND as a mantissa
+    PROC
+    LOCAL RND_LOOP
 
-	call RAND	
+    call RAND
+    ; BC = HL since ZX BASIC uses ED CB A registers for FP
     ld b, h
     ld c, l
 
@@ -77,34 +78,34 @@ RND:
     or d
     or c
     or b
-    ret z   ; Returns 0 if HL=DE=0
+    ret z   ; Returns 0 if BC=DE=0
 
-	; We already have a random 32 bit mantissa in ED CB
+    ; We already have a random 32 bit mantissa in ED CB
     ; From 0001h to FFFFh
 
-	ld l, 81h	; Exponent
-	; At this point we have [0 .. 1) FP number;
+    ld l, 81h	; Exponent
+    ; At this point we have [0 .. 1) FP number;
 
-	; Now we must shift mantissa left until highest bit goes into carry
-    ld a, e ; Use a register for the higher byte for faster RR
+    ; Now we must shift mantissa left until highest bit goes into carry
+    ld a, e ; Use A register for rotating E faster (using RLA instead of RL E)
 RND_LOOP:
-	dec l
-	sla b
-	rl c
-	rl d
-	rla
-	jp nc, RND_LOOP
+    dec l
+    sla b
+    rl c
+    rl d
+    rla
+    jp nc, RND_LOOP
 
-    ; Now undo last mantissa left-shif
-    ccf ; Clears flag (it was set) to insert a 0 bit -> positive number
+    ; Now undo last mantissa left-shift once
+    ccf ; Clears carry to insert a 0 bit back into mantissa -> positive FP number
     rra
     rr d   
     rr c
     rr b
     
-	ld e, a     ; E must have the highest byte
+    ld e, a     ; E must have the highest byte
     ld a, l     ; exponent in A
-	ret
+    ret
 
-	ENDP
+    ENDP
 
