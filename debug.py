6e91e628cfb60d7dc6932df71369fa4486e682a0
boriel <boriel@localhost>
2012-10-24 21:30:16 +0100

    Tabs removed. Tabs replaced by 4 spaces.

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# vim:ts=4:sw=4:et:
 
 # Simple debugging module
 
@@ -12,21 +13,21 @@ from common import OPTIONS
 import inspect
 
 def __DEBUG__(msg, level = 1):
-	if level > OPTIONS.Debug.value:
-		return
+    if level > OPTIONS.Debug.value:
+        return
 
-	line = inspect.getouterframes(inspect.currentframe())[1][2]
-	OPTIONS.stderr.value.write('debug: %i: %s\n' % (line, msg))
+    line = inspect.getouterframes(inspect.currentframe())[1][2]
+    OPTIONS.stderr.value.write('debug: %i: %s\n' % (line, msg))
 
 
 def __LINE__():
-	''' Returns current file interpreter line
-	'''
-	return inspect.getouterframes(inspect.currentframe())[1][2]
-	
+    ''' Returns current file interpreter line
+    '''
+    return inspect.getouterframes(inspect.currentframe())[1][2]
+    
 
 def __FILE__():
-	''' Returns current file interpreter line
-	'''
-	return inspect.currentframe().f_code.co_filename
+    ''' Returns current file interpreter line
+    '''
+    return inspect.currentframe().f_code.co_filename
 
