fcd95df5ddc5fc85c6744ad73ed817a8f450b5dd
boriel <boriel@localhost>
2012-11-12 00:12:42 +0000

    Useles code removed. This code is useless, since I misunderstood the ROM FP-Calc problem. Removed.

--- library-asm/pow.asm
+++ library-asm/pow.asm
@@ -9,11 +9,6 @@
 ;
 ; Uses CALLEE convention
 ;
-; Since the ROM uses LN to calculate this, exponents must be
-; positive (Try 2^-1 in your speccy)
-; We can fix this by checking in advance if exponent is < 0
-; and returning 1 / (B ^ abs(e))
-;
 ; Operands comes swapped:
 ; 	1 st parameter is the BASE (A ED CB)
 ;   2 nd parameter (Top of the stack) is Exponent
@@ -22,37 +17,6 @@
 __POW:	; Exponentiation
 	PROC
 	
-	LOCAL __POSITIVE
-
-	bit 7, e
-	jr z, __POSITIVE
-	res 7, e ; Make exponent positive
-
-	push bc  ; Preserve exponent
-	push de
-	push af
-
-	rst 28h
-	defb 0A1h ; Push 1	 => Base, 1
-	defb 38h ; END CALC
-
-	pop af	 ; Recover current number
-	pop de
-	pop bc
-
-	call __FPSTACK_PUSH2
-	
-	; ------------- ROM POW
-	rst 28h
-	defb 01h  	; Exchange => 1, Base
-	defb 06h	; POW
-	defb 05h	; DIV
-	defb 38h;   ; END CALC
-	
-	jp __FPSTACK_POP
-
-
-__POSITIVE:	; Proceed as always
 	call __FPSTACK_PUSH2
 	
 	; ------------- ROM POW
