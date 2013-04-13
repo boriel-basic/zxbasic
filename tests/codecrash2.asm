dcbda07f33f093c511f913b9963331cc8e7d6e92
Jose Rodriguez <boriel@users.noreply.github.com>
2013-04-13 14:37:45 +0100

    Test updated. Test updated to reflect new error.asm constant.

--- tests/codecrash2.asm
+++ tests/codecrash2.asm
@@ -133,6 +133,7 @@ __CALL_BACK__:
 	ERROR_NumberTooBig      EQU     5
 	ERROR_InvalidArg        EQU     9
 	ERROR_IntOutOfRange     EQU    10
+	ERROR_InvalidFileName   EQU    14 
 	ERROR_InvalidColour     EQU    19
 	ERROR_BreakIntoProgram  EQU    20
 	ERROR_TapeLoadingErr    EQU    26
--- tests/strsigil.asm
+++ tests/strsigil.asm
@@ -279,6 +279,7 @@ __STRLEN:	; Direct FASTCALL entry
 	ERROR_NumberTooBig      EQU     5
 	ERROR_InvalidArg        EQU     9
 	ERROR_IntOutOfRange     EQU    10
+	ERROR_InvalidFileName   EQU    14 
 	ERROR_InvalidColour     EQU    19
 	ERROR_BreakIntoProgram  EQU    20
 	ERROR_TapeLoadingErr    EQU    26
