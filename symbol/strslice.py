7eca88b8a786bde34ad6e1315646b816a059d08e
Jose Rodriguez <boriel@users.noreply.github.com>
2013-05-11 17:25:15 +0100

    Symbol base class now adds children on constructor initialization. Better legibility of binary and other classes. All class methods are now called make_node( )

--- symbol/strslice.py
+++ symbol/strslice.py
@@ -9,8 +9,19 @@
 #                    the GNU General License
 # ----------------------------------------------------------------------
 
-from symbol import Symbol
 
+from symbol import Symbol
+from number import SymbolNUMBER as NUMBER
+from typecast import SymbolTYPECAST as TYPECAST
+from binary import SymbolBINARY as BINARY
+from string_ import SymbolSTRING as STRING
+
+from config import OPTIONS
+from api.check import check_type
+from api.helpers import is_number
+
+from api.constants import MIN_STRSLICE_IDX
+from api.constants import MAX_STRSLICE_IDX
 
 class SymbolSTRSLICE(Symbol):
     ''' Defines a string slice
@@ -27,26 +38,64 @@ class SymbolSTRSLICE(Symbol):
     @string.setter
     def string(self, value):
         self.children[0] = value
-        
+
     @property
     def lower(self):
         return self.children[1]
-       
+
     @lower.setter
     def lower(self, value):
         self.children[1] = value
-        
+
     @property
     def upper(self):
         return self.children[2]
-        
+
     @upper.setter
     def upper(self, value):
         self.children[2] = value
-    
 
-'''    @classmethod
-    def 
-    
-    '''
-    
+    @classmethod
+    def make_node(clss, lineno, s, lower, upper):
+        ''' Creates a node for a string slice. S is the string expression Tree.
+        Lower and upper are the bounds, if lower & upper are constants, and
+        s is also constant, s, then a string constant is returned.
+
+        If lower > upper, an empty string is returned.
+        '''
+        if not check_type(lineno, 'string', s):
+            return None
+
+        lo = up = None
+        base = NUMBER(OPTIONS.string_base.value, lineno=lineno)
+
+        lower = TYPECAST.make_node('u16',
+                    BINARY.make_node('MINUS', lower, base, lineno=lineno,
+                                     func=lambda x, y: x - y), lineno)
+        upper = TYPECAST.make_node('u16',
+                    BINARY.make_node('MINUS', upper, base, lineno=lineno,
+                                     func=lambda x, y: x - y), lineno)
+        if is_number(lower):
+            lo = lower.value
+            if lo < MIN_STRSLICE_IDX:
+                lower.value = lo = MIN_STRSLICE_IDX
+
+        if is_number(upper):
+            up = upper.value
+            if up > MAX_STRSLICE_IDX:
+                upper.value = up = MAX_STRSLICE_IDX
+
+        if is_number(lower, upper):
+            if lo > up:
+                return STRING('', lineno)
+
+            if s.token == 'STRING':  # A constant string? Recalculate it now
+                up += 1
+                st = s.value.ljust(up)  # Procrustean filled (right)
+                return STRING(st[lo:up], lineno)
+
+            # a$(0 TO INF.) = a$
+            if lo == MIN_STRSLICE_IDX and up == MAX_STRSLICE_IDX:
+                return s
+
+        return clss(s, lower, upper, lineno)
--- symbol/typecast.py
+++ symbol/typecast.py
@@ -24,7 +24,7 @@ class SymbolTYPECAST(Symbol):
         Symbol.__init__(self, operand)
         self.lineno = lineno
         self.type_ = new_type
-        
+
     ## The converted (typecast) node
     @property
     def operand(self):
@@ -35,56 +35,56 @@ class SymbolTYPECAST(Symbol):
         self.children[0] = operand_
 
     @classmethod
-    def make_typecast(class_, new_type, node, lineno):
+    def make_node(class_, new_type, node, lineno):
         ''' Creates a node containing the type cast of
         the given one. If new_type == node.type, then
         nothing is done, and the same node is
         returned.
-    
+
         Returns None on failure (and calls syntax_error)
         '''
         # None (null) means the given AST node is empty (usually an error)
         if node is None:
             return None # Do nothing. Return as is
-    
+
         # The source and dest types are the same
         if new_type == node.type_:
             return node # Do nothing. Return as is
-    
+
         # Typecasting, at the moment, only for number
         if node.type_ == 'string':
             syntax_error(lineno, 'Cannot convert string to a value. Use VAL() function')
             return None
-    
+
         # Converting from string to number is done by STR
         if new_type == 'string':
             syntax_error(lineno, 'Cannot convert value to string. Use STR() function')
             return None
-    
+
         # If the given operand is a constant, perform a static typecast
         if is_const(node.symbol):
             node = node.expr
-    
+
         if not is_number(node):
             return class_(new_type, node, lineno)
-    
+
         # It's a number. So let's convert it directly
         if node.token != 'NUMBER':
             if node.class_ == 'const':
                 node = SymbolNUMBER(node.value, node.type_, node.lineno)
-    
-        if new_type not in ('i8', 'u8', 'i16', 'u16', 'i32', 'u32'): # not an integer
+
+        if new_type not in ('i8', 'u8', 'i16', 'u16', 'i32', 'u32'):  # not an integer
             node.value = float(node.value)
-        else: # It's an integer
-            new_val = int(node.value) & ((1 << (8 * TYPE_SIZES[new_type])) - 1) # Mask it
-    
+        else:  # It's an integer
+            new_val = int(node.value) & ((1 << (8 * TYPE_SIZES[new_type])) - 1)  # Mask it
+
             if node.value >= 0 and new_val != node.value:
                 warning_conversion_lose_digits(node.symbol.lineno)
                 node.value = new_val
-            elif node.value < 0 and (1 << (TYPE_SIZES[new_type] * 8)) + node.value != new_val:
+            elif node.value < 0 and (1 << (TYPE_SIZES[new_type] * 8)) + \
+                    node.value != new_val:
                 warning_conversion_lose_digits(node.symbol.lineno)
                 node.value = new_val - (1 << (TYPE_SIZES[new_type] * 8))
-    
+
         node.type_ = new_type
         return node
-
