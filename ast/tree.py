49d0406d75aa35a2c56d98fd0919732e2b89a4a1
Jose Rodriguez <boriel@users.noreply.github.com>
2013-05-15 00:40:37 +0100

    Clean up

--- ast/tree.py
+++ ast/tree.py
@@ -6,6 +6,7 @@ __all__ = ['NotAnAstError', 'Tree']
 import copy
 from api.errors import Error
 
+
 class NotAnAstError(Error):
     ''' Thrown when the "pointer" is not
     an AST, but another thing.
@@ -18,14 +19,13 @@ class NotAnAstError(Error):
         return self.msg
 
 
-
 class Tree(object):
     ''' Simple tree implementation
     '''
     def __init__(self):
         self.children = []
 
-    def inorder(self, funct, stopOn = None):
+    def inorder(self, funct, stopOn=None):
         ''' Iterates in order, calling the function with the current node.
         If stopOn is set to True or False, it will stop on true or false.
         '''
@@ -39,24 +39,22 @@ class Tree(object):
 
         return funct(self)
 
-
-    def preorder(self, funct, stopOn = None):
+    def preorder(self, funct, stopOn=None):
         ''' Iterates in preorder, calling the function with the current node.
-            If stopOn is set to True or False, it will stop on true or false.   
+            If stopOn is set to True or False, it will stop on true or false.
         '''
         if funct(self.symbol) == stopOn and stopOn is not None:
             return stopOn
 
-        if stopOn is None:        
+        if stopOn is None:
             for i in self.children:
                 i.preorder(funct)
         else:
             for i in self.children:
                 if i.preorder(funct) == stopOn:
                     return stopOn
-                    
 
-    def postorder(self, funct, stopOn = None):
+    def postorder(self, funct, stopOn=None):
         ''' Iterates in postorder, calling the function with the current node.
         If stopOn is set to True or False, it will stop on true or false.
         '''
@@ -67,21 +65,17 @@ class Tree(object):
             for i in range(len(self.children) - 1, -1, -1):
                 if self.children[i].postorder(funct) == stopOn:
                     return stopOn
-
         return funct(self.symbol)
-        
-        
+
     def appendChild(self, node):
         ''' Appends the given node to the current children list
         '''
         self.children.append(node)
 
-
     def prependChild(self, node):
         ''' Inserts the given node at the beginning of the children list
         '''
         self.children.insert(0, node)
-
 
     @classmethod
     def makenode(clss, symbol, *nexts):
@@ -98,10 +92,8 @@ class Tree(object):
 
         return result
 
-
     def __deepcopy(self, memo):
-        result = Tree(self.symbol) # No need to duplicate the symbol memory
+        result = Tree(self.symbol)  # No need to duplicate the symbol memory
         result.next = copy.deepcopy(self.children)
 
         return result
-
