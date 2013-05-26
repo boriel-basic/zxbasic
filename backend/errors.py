4850c2f0f1528e77367b6b5ce2df23f2542dd80d
Jose Rodriguez <boriel@users.noreply.github.com>
2013-05-26 22:50:08 +0100

    Fixes some import. Reimports tokens for PLY to work.

--- backend/errors.py
+++ backend/errors.py
@@ -14,15 +14,15 @@ __all__ = ['GenericError',
 class GenericError(Error):
     ''' Backend Generic ERROR
     '''
-    def __init__(self, msg = None):
+    def __init__(self, msg=None):
         if msg is None:
             msg = 'Generic Backend Internal Error. Plase, report this'
 
         self.msg = msg
-        
+
     def __str__(self):
         return self.msg
-        
+
     def __repr__(self):
         return '%s: %s' % (self.__class__.__name__, self.msg)
 
@@ -30,13 +30,13 @@ class GenericError(Error):
 class InvalidICError(GenericError):
     ''' Invalid Intermediate Code instruction
     '''
-    def __init__(self, ic, msg = None):
+    def __init__(self, ic, msg=None):
         if msg is None:
             msg = 'Invalid intermediate code instruction "%s"' % ic
 
         GenericError.__init__(self, msg)
         self.ic = ic
-   
+
 
 class NoMoreRegistersError(GenericError):
     ''' Raised when no more assigned register are available.
@@ -64,19 +64,17 @@ class TempAlreadyFreedError(GenericError):
 # -----------------------------------------------------------------------------
 #  Functions for throwing errors
 # -----------------------------------------------------------------------------
-     
 def throw_invalid_quad_code(quad):
     ''' Exception raised when an invalid quad code has been emmitted.
     '''
     raise InvalidICError(str(quad))
-    
+
 
 def throw_invalid_quad_params(quad, QUADS, nparams):
     ''' Exception raised when an invalid number of params in the
         quad code has been emmitted.
     '''
     raise InvalidICError(str(quad),
-            "Invalid quad code params for '%s' (expected %i, but got %i)" % 
+            "Invalid quad code params for '%s' (expected %i, but got %i)" %
             (quad, QUADS[quad][0], nparams)
         )
-
