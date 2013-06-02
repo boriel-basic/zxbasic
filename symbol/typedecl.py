32d21dd0a2a413fa5bf613b96680f3ec2cc1a09f
boriel <boriel@localhost>
2013-06-03 00:38:52 +0100

    Refact: uses TYPE constants. __str__() and __repr__() defined.

--- symbol/typedecl.py
+++ symbol/typedecl.py
@@ -9,7 +9,7 @@
 #                    the GNU General License
 # ----------------------------------------------------------------------
 
-from api.constants import TYPE_SIZES
+from api.constants import TYPE
 from symbol import Symbol
 
 
@@ -23,6 +23,15 @@ class SymbolTYPEDECL(Symbol):
         '''
         Symbol.__init__(self)
         self.type_ = type_
-        self.size = TYPE_SIZES[self.type_]
         self.lineno = lineno
         self.implicit = implicit
+
+    @property
+    def size(self):
+        return TYPE.size(self.type_)
+
+    def __str__(self):
+        return TYPE.to_string(self.type_)
+
+    def __repr__(self):
+        return "%s(%s)" % (self.token, str(self))
