45c5ea8cb10db092ba8efede33527884bd186971
Jose Rodriguez <boriel@users.noreply.github.com>
2013-12-18 23:49:30 +0000

    Added test for ne and eq

--- tests/symbols/test_symbolBASICTYPE.py
+++ tests/symbols/test_symbolBASICTYPE.py
@@ -22,6 +22,26 @@ class TestSymbolBASICTYPE(TestCase):
             t = SymbolBASICTYPE(TYPE.to_string(type_), type_)
             self.assertTrue(t.is_basic)
 
+    def test__eq__(self):
+        for t_ in TYPE.types:
+            t = SymbolBASICTYPE(TYPE.to_string(t_), t_)
+            self.assertTrue(t == t)  # test same reference
+
+        for t_ in TYPE.types:
+            t1 = SymbolBASICTYPE(TYPE.to_string(t_), t_)
+            t2 = SymbolBASICTYPE(TYPE.to_string(t_), t_)
+            self.assertTrue(t1 == t2)
+
+    def test__ne__(self):
+        for t1_ in TYPE.types:
+            t1 = SymbolBASICTYPE(TYPE.to_string(t1_), t1_)
+            for t2_ in TYPE.types:
+                t2 = SymbolBASICTYPE(TYPE.to_string(t2_), t2_)
+                if t1 == t2:  # Already validated
+                    self.assertTrue(t1 == t2)
+                else:
+                    self.assertTrue(t1 != t2)
+
 
 if __name__ == '__main__':
     unittest.main()
