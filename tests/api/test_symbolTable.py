a986e076043baebcb9f2a089b40654e69351064a
boriel <boriel@localhost>
2013-12-21 00:43:37 +0000

    Added more declare_variable tests Uses mocking (StringIO instead of stderr)

--- tests/api/test_symbolTable.py
+++ tests/api/test_symbolTable.py
@@ -3,12 +3,14 @@
 
 import unittest
 from unittest import TestCase
+from StringIO import StringIO
 
 # Initialize import syspath
 import __init__
 
 from api.symboltable import SymbolTable
 from api.constants import TYPE
+from api.config import OPTIONS
 from symbols.type_ import SymbolTYPE
 from symbols.type_ import SymbolBASICTYPE
 
@@ -24,7 +26,35 @@ class TestSymbolTable(TestCase):
 
         self.assertEqual(s.current_scope, s.global_scope)
 
+    def test_declare_variable(self):
+        OPTIONS.stderr.value = StringIO()
+        s = SymbolTable()
+        # Checks variable 'a' is not declared yet
+        self.assertFalse(s.check_is_declared('a', 0, 'var', show_error=False))
+        # Checks variable 'a' is undeclared
+        self.assertTrue(s.check_is_undeclared('a', show_error=False))
+
+        # Declares a variable named 'a' (produces duplicated name error)
+        s.declare_variable('a', 10, s.basic_types[TYPE.to_string(TYPE.integer)])
+        # Checks variable 'a' is declared
+        self.assertTrue(s.check_is_declared('a', 1, 'var', show_error=False))
+        # Checks variable 'a' is not undeclared
+        self.assertFalse(s.check_is_undeclared('a', show_error=False))
+        # Now checks for duplicated name 'a'
+        s.declare_variable('a', 10, s.basic_types[TYPE.to_string(TYPE.integer)])
+        self.assertEqual(OPTIONS.stderr.value.getvalue(),
+                         "(stdin):10: Variable 'a' already declared at (stdin):10\n")
+
+    def test_nested_scope(self):
+        s = SymbolTable()
+         # Declares a variable named 'a'
+        s.declare_variable('a', 10, s.basic_types[TYPE.to_string(TYPE.integer)])
+        s.start_function_body('testfunction')
+        # Now checks for duplicated name 'a'
+        s.declare_variable('a', 10, s.basic_types[TYPE.to_string(TYPE.float_)])
 
 
 if __name__ == '__main__':
+    OPTIONS.remove_option('stderr')
+    OPTIONS.add_option('stderr', default_value=StringIO())
     unittest.main()
