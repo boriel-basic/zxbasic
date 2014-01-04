e1b62a645aec7d47e4dd6d14acffa16208f7c696
Jose Rodriguez <boriel@gmail.com>
2014-01-04 23:24:23 +0000

    Test for SymbolVar finally added.

--- tests/symbols/test_symbolVAR.py
+++ tests/symbols/test_symbolVAR.py
@@ -9,6 +9,8 @@ import __init__
 
 import symbols
 from symbols.type_ import Type
+from api.constants import SCOPE
+from api.constants import KIND
 
 
 class TestSymbolVAR(TestCase):
@@ -19,33 +21,43 @@ class TestSymbolVAR(TestCase):
         self.assertRaises(AssertionError, symbols.VAR, 'v', 1, None, 'blah')  # type_='blah'
 
     def test_size(self):
-        self.assertisNone(self.v.type_)
+        self.assertIsNone(self.v.type_)
         self.v.type_ = Type.byte_
         self.assertEqual(self.v.type_, Type.byte_)
 
     def test_kind(self):
-        self.fail()
+        self.assertEqual(self.v.kind, KIND.var)
 
     def test_set_kind(self):
-        self.fail()
+        self.v.set_kind(KIND.function, 2)
+        self.assertEqual(self.v.kind, KIND.function)
+
+    def test_set_kind_fail(self):
+        self.assertRaises(AssertionError, self.v.set_kind, 'blah', 2)
 
     def test_add_alias(self):
-        self.fail()
+        self.v.add_alias(self.v)
 
-    def test_make_alias(self):
-        self.fail()
+    def test_add_alias_fail(self):
+        self.assertRaises(AssertionError, self.v.add_alias, 'blah')
 
     def test_is_aliased(self):
-        self.fail()
+        self.assertFalse(self.v.is_aliased)
+        self.v.add_alias(self.v)
+        self.assertTrue(self.v.is_aliased)
 
     def test_t(self):
-        self.fail()
+        self.assertEqual(self.v.scope, SCOPE.global_)  # Must be initialized as global_
+        self.assertEqual(self.v.t, self.v.mangled)
+        self.v.scope = SCOPE.local
+        self.assertEqual(self.v.t, self.v._t)
 
     def test_type_(self):
-        self.fail()
+        self.v.type_ = Type.byte_
+        self.assertEqual(self.v.type_, Type.byte_)
 
-    def test_type_(self):
-        self.fail()
+    def test_type_fail(self):
+        self.assertRaises(AssertionError, symbols.VAR.type_.fset, self.v, 'blah')
 
 
 if __name__ == '__main__':
