1cbac352171951dce0773bc7cb4c42e55a83389e
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-05 11:08:49 +0000

    SymbolARGLIST unit test added.

---
+++ tests/symbols/test_symbolARGLIST.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+from unittest import TestCase
+
+# Initialize import syspath
+import __init__
+
+import symbols
+
+class TestSymbolARGLIST(TestCase):
+    def setUp(self):
+        self.VALUE = 5
+        self.a = symbols.ARGLIST(symbols.ARGUMENT(symbols.NUMBER(self.VALUE, 1), 1))
+
+    def test__len__(self):
+        self.assertEqual(len(self.a), 1)
+        b = symbols.ARGLIST()
+        self.assertEqual(len(b), 0)
+
+    def test_args(self):
+        self.assertEqual(self.a[0], self.VALUE)
+
+    def test_args_setter(self):
+        self.a[0] = symbols.ARGUMENT(symbols.NUMBER(self.VALUE + 1, 1), 1)
+        self.assertEqual(self.a[0], self.VALUE + 1)
+
+    def test_args_setter_fail(self):
+        self.assertRaises(AssertionError, symbols.ARGLIST.__setitem__, self.a, 0, 'blah')
+
+    def test_make_node_empty(self):
+        b = symbols.ARGLIST.make_node(None)
+        self.assertIsInstance(b, symbols.ARGLIST)
+        self.assertEqual(len(b), 0)
+
+    def test_make_node_single(self):
+        b = symbols.ARGLIST.make_node(symbols.ARGUMENT(symbols.NUMBER(self.VALUE, 1), 1))
+        self.assertIsInstance(b, symbols.ARGLIST)
+        self.assertEqual(len(b), 1)
+        self.assertEqual(b[0], self.VALUE)
+
+    def test_make_node_single2(self):
+        b = symbols.ARGLIST.make_node(None, symbols.ARGUMENT(symbols.NUMBER(self.VALUE, 1), 1))
+        self.assertIsInstance(b, symbols.ARGLIST)
+        self.assertEqual(len(b), 1)
+        self.assertEqual(b[0], self.VALUE)
+
+    def test_make_node_fails(self):
+        self.assertRaises(AssertionError, symbols.ARGLIST.make_node, 'blah')
+
+
+if __name__ == '__main__':
+    unittest.main()
