228e1a9545a29e515710e3d32110c0e3d0cf3d62
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-08 23:27:38 +0000

    Added test 39. Initialized local variables.

---
+++ tests/functional/39.asm
@@ -0,0 +1,192 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld a, 5
+	push af
+	call _test
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_test:
+	push ix
+	ld ix, 0
+	add ix, sp
+	ld hl, 0
+	push hl
+	inc sp
+	push ix
+	pop hl
+	ld bc, -1
+	add hl, bc
+	ex de, hl
+	ld hl, __LABEL0
+	ld bc, 1
+	ldir
+	ld a, (ix+5)
+	push af
+	ld a, (ix-1)
+	ld h, a
+	pop af
+	add a, h
+	call __U8TOFREG
+_test__leave:
+	ld sp, ix
+	pop ix
+	exx
+	pop hl
+	ex (sp), hl
+	exx
+	ret
+#line 1 "u32tofreg.asm"
+#line 1 "neg32.asm"
+__ABS32:
+		bit 7, d
+		ret z
+	
+__NEG32: ; Negates DEHL (Two's complement)
+		ld a, l
+		cpl
+		ld l, a
+	
+		ld a, h
+		cpl
+		ld h, a
+	
+		ld a, e
+		cpl
+		ld e, a
+		
+		ld a, d
+		cpl
+		ld d, a
+	
+		inc l
+		ret nz
+	
+		inc h
+		ret nz
+	
+		inc de
+		ret
+	
+#line 2 "u32tofreg.asm"
+__I8TOFREG:
+		ld l, a
+		rlca
+		sbc a, a	; A = SGN(A)
+		ld h, a
+		ld e, a
+		ld d, a
+	
+__I32TOFREG:	; Converts a 32bit signed integer (stored in DEHL)
+					; to a Floating Point Number returned in (A ED CB)
+	
+		ld a, d
+		or a		; Test sign
+	
+		jp p, __U32TOFREG	; It was positive, proceed as 32bit unsigned
+	
+		call __NEG32		; Convert it to positive
+		call __U32TOFREG	; Convert it to Floating point
+	
+		set 7, e			; Put the sign bit (negative) in the 31bit of mantissa
+		ret
+	
+__U8TOFREG:
+					; Converts an unsigned 8 bit (A) to Floating point
+		ld l, a
+		ld h, 0
+		ld e, h
+		ld d, h
+	
+__U32TOFREG:	; Converts an unsigned 32 bit integer (DEHL)
+					; to a Floating point number returned in A ED CB
+	
+	    PROC
+	
+	    LOCAL __U32TOFREG_END
+	
+		ld a, d
+		or e
+		or h
+		or l
+	    ld b, d
+		ld c, e		; Returns 00 0000 0000 if ZERO
+		ret z
+	
+		push de
+		push hl
+	
+		exx
+		pop de  ; Loads integer into B'C' D'E' 
+		pop bc
+		exx
+	
+		ld l, 128	; Exponent
+		ld bc, 0	; DEBC = 0
+		ld d, b
+		ld e, c
+	
+__U32TOFREG_LOOP: ; Also an entry point for __F16TOFREG
+		exx
+		ld a, d 	; B'C'D'E' == 0 ?
+		or e
+		or b
+		or c
+		jp z, __U32TOFREG_END	; We are done
+	
+		srl b ; Shift B'C' D'E' >> 1, output bit stays in Carry
+		rr c
+		rr d
+		rr e
+		exx
+	
+		rr e ; Shift EDCB >> 1, inserting the carry on the left
+		rr d
+		rr c
+		rr b
+	
+		inc l	; Increment exponent
+		jp __U32TOFREG_LOOP
+	
+	
+__U32TOFREG_END:
+		exx
+	    ld a, l     ; Puts the exponent in a
+		res 7, e	; Sets the sign bit to 0 (positive)
+	
+		ret
+	    ENDP
+	
+#line 51 "39.bas"
+	
+ZXBASIC_USER_DATA:
+__LABEL0:
+	DEFB 02h
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
