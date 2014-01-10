42010776efee4e6271239907165be86516f4abab
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-10 21:57:14 +0000

    - Bugfixes (bugs reintroduced during string folding) - Message error improvement

--- api/decorator.py
+++ api/decorator.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import collections
+import inspect
+import os
 
 
 class classproperty(object):
@@ -19,37 +21,48 @@ class classproperty(object):
 def check_type(*args, **kwargs):
     ''' Checks the function types
     '''
-    args = (x if isinstance(x, collections.Iterable) else (x,) for x in args)
+    args = tuple(x if isinstance(x, collections.Iterable) else (x,) for x in args)
     kwargs = {x: kwargs[x] if isinstance(kwargs[x], collections.Iterable) else (kwargs[x],) for x in kwargs}
 
     def decorate(func):
         types = args
         kwtypes = kwargs
+        gi = "Got <{}> instead"
+        errmsg1 = "to be of type <{}>. " + gi
+        errmsg2 = "to be one of type ({}). " + gi
+        errar = "{}:{} expected '{}' "
+        errkw = "{}:{} expected {} "
 
         def check(*ar, **kw):
+            line = inspect.getouterframes(inspect.currentframe())[1][2]
+            fname = os.path.basename(inspect.getouterframes(inspect.currentframe())[1][1])
+
             for arg, type_ in zip(ar, types):
                 if type(arg) not in type_:
-                    if len(type_) > 1:
-                        raise TypeError("Expected '{}' to be one of type {}. "
-                                        "Got {} instead".format(arg, tuple(type_), type(arg)))
+                    if len(type_) == 1:
+                        raise TypeError((errar + errmsg1).format(fname, line, arg, type_[0].__name__,
+                                                                 type(arg).__name__))
                     else:
-                        raise TypeError("Expected '{}' to be of type {}. "
-                                        "Got {} instead".format(arg, type_[0], type(arg)))
+                        raise TypeError((errar + errmsg2).format(fname, line, arg,
+                                                                 ', '.join('<%s>' % x.__name__ for x in type_),
+                                                                 type(arg).__name__))
             for kwarg in kw:
                 if kwtypes.get(kwarg, None) is None:
                     continue
-
                 if type(kw[kwarg]) not in kwtypes[kwarg]:
-                    if len(kwtypes[kwarg]) > 1:
-                        raise TypeError("Expected parameter {} to be one of type {}. "
-                                        "Got {} instead".format(kwarg, tuple(kwtypes[kwarg]), type(kw[kwarg])))
+                    if len(kwtypes[kwarg]) == 1:
+                        raise TypeError((errkw + errmsg1).format(fname, line, kwarg, kwtypes[kwarg][0].__name__,
+                                                                 type(kw[kwarg]).__name__))
                     else:
-                        raise TypeError("Expected parameter {} to be of type {}. "
-                                        "Got {} instead".format(kwarg, kwtypes[kwarg][0], type(kw[kwarg])))
+                        raise TypeError((errkw + errmsg2).format(fname, line, kwarg,
+                                                                 ', '.join('<%s>' % x.__name__
+                                                                                  for x in kwtypes[kwarg]),
+                                                                 type(kw[kwarg]).__name__))
 
             return func(*ar, **kw)
 
         return check
 
     return decorate
+
 
