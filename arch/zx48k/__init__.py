8b7382f88c313197f91cbec4cb16b21c887d8565
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-11 15:06:46 +0000

    More arch settings initialization.

--- arch/zx48k/__init__.py
+++ arch/zx48k/__init__.py
@@ -15,4 +15,7 @@ from api.constants import TYPE
 api.global_.PARAM_ALIGN = 2  # Z80 param align
 api.global_.BOUND_TYPE = TYPE.uinteger
 api.global_.SIZE_TYPE = TYPE.ubyte
-
+api.global_.PTR_TYPE = TYPE.uinteger
+api.global_.STR_INDEX_TYPE = TYPE.uinteger
+api.global_.MIN_STRSLICE_IDX = 0      # Min. string slicing position
+api.global_.MAX_STRSLICE_IDX = 65534  # Max. string slicing position
