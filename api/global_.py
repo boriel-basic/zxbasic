d89d24abc064ebb40c2d88f056681f81b6a5c023
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-11 08:30:10 +0000

    Added SIZE_TYPE. This type is internal to the compiler and it?s used to describe type sizes, and elements sizes. E.g. size_of(TYPE.ubyte) == 1, type_of(size_of(TYPE.ubyte)) = TYPE.ubyte This is a global constant set by the arch. configuration. For Z80 = 1

--- api/global_.py
+++ api/global_.py
@@ -101,3 +101,9 @@ PARAM_ALIGN = None  # Set to None, so if not set will raise error
 # Data type used for array boundaries. Must be an integral
 # ----------------------------------------------------------------------
 BOUND_TYPE = None  # Set to None, so if not set will raise error
+
+# ----------------------------------------------------------------------
+# Data type used for elements size. Must be an integral
+# ----------------------------------------------------------------------
+SIZE_TYPE = None
+
