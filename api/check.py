520212be63da8663730852cbe479c2e93b2bcfe8
boriel <boriel@localhost>
2014-01-12 22:54:34 +0000

    check_is_declared reverted back and renamed to check_is_declared_strict

--- api/check.py
+++ api/check.py
@@ -17,6 +17,7 @@ from errmsg import syntax_error
 
 
 __all__ = ['check_type',
+           'check_is_declared_strict',
            'check_call_arguments',
            'check_pending_calls',
            'check_pending_labels',
@@ -50,6 +51,21 @@ def check_type(lineno, type_list, arg):
                      % (arg.type_, tuple(type_list)))
 
     return False
+
+
+def check_is_declared_strict(lineno, id_, classname='variable'):
+    ''' Check if the current ID is already declared.
+    If not, triggers a "undeclared identifier" error,
+    if the --strict command line flag is enabled (or #pragma
+    option strict is in use).
+
+    If not in strict mode, passes it silently.
+    '''
+    if not config.OPTIONS.explicit.value:
+        return True
+
+    entry = global_.SYMBOL_TABLE.check_is_declared(id_, lineno, classname)
+    return entry is not None  # True if declared
 
 
 def check_call_arguments(lineno, id_, args):
@@ -99,7 +115,7 @@ def check_pending_calls():
     '''
     result = True
 
-    # Check for functions defined after calls (parameters, etc)
+    # Check for functions defined after calls (parametres, etc)
     for id_, params, lineno in global_.FUNCTION_CALLS:
         result = result and check_call_arguments(lineno, id_, params)
 
