cca3f0a4c528b8e772af184fe309ba1948255628
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-14 20:08:19 +0000

    Added test 65 Let a = Test(1, 2)  // FASTCALLed Func call Must emit a Warning

---
+++ tests/functional/66.asm
@@ -0,0 +1,52 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld a, 082h
+	ld de, 00000h
+	ld bc, 00000h
+	push bc
+	push de
+	push af
+	ld a, 1
+	call _test
+	ld (_a), a
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_test:
+	ld a, (ix+5)
+	inc a
+	ld (ix+5), a
+_test__leave:
+	ret
+	
+ZXBASIC_USER_DATA:
+_a:
+	DEFB 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
---
+++ tests/functional/66.bas
@@ -0,0 +1,7 @@
+
+Function FASTCALL test(a as Ubyte, b as Float) as uByte
+    LET a = a + 1
+end Function
+
+let a = test(1, 2)
+
