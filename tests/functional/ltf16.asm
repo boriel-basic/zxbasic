5ec33f9a8211a4902ba5a54861b8019776b710cb
boriel <boriel@localhost>
2014-01-18 09:47:21 +0000

    Test LTF16 updated.

--- tests/functional/ltf16.asm
+++ tests/functional/ltf16.asm
@@ -186,6 +186,11 @@ __SWAP32:
 #line 76 "ltf16.bas"
 	
 ZXBASIC_USER_DATA:
+_level:
+	DEFB 00h
+	DEFB 00h
+	DEFB 01h
+	DEFB 00h
 _le:
 	DEFB 00h
 	DEFB 00h
@@ -193,11 +198,6 @@ _le:
 	DEFB 00h
 _l:
 	DEFB 00, 00, 00, 00
-_level:
-	DEFB 00h
-	DEFB 00h
-	DEFB 01h
-	DEFB 00h
 	; Defines DATA END --> HEAP size is 0
 ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
 	; Defines USER DATA Length in bytes
