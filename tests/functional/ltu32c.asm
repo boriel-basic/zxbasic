02238bb7eb22d8ad7678b42d8a2350ee366fa801
boriel <boriel@localhost>
2014-01-18 09:48:09 +0000

    Test LTI32 updated. Test LTU32 updated.

--- tests/functional/ltu32c.asm
+++ tests/functional/ltu32c.asm
@@ -162,6 +162,11 @@ __SWAP32:
 #line 94 "ltu32c.bas"
 	
 ZXBASIC_USER_DATA:
+_level:
+	DEFB 01h
+	DEFB 00h
+	DEFB 00h
+	DEFB 00h
 _le:
 	DEFB 00h
 	DEFB 00h
@@ -169,11 +174,6 @@ _le:
 	DEFB 00h
 _l:
 	DEFB 00, 00, 00, 00
-_level:
-	DEFB 01h
-	DEFB 00h
-	DEFB 00h
-	DEFB 00h
 	; Defines DATA END --> HEAP size is 0
 ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
 	; Defines USER DATA Length in bytes
