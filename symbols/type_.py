d2e75b0970c3314462081cbcd271f13f60171f7f
boriel <boriel@localhost>
2014-01-21 22:20:32 +0000

    TYPE objects now have .accessed attrib.

--- symbols/type_.py
+++ symbols/type_.py
@@ -29,6 +29,7 @@ class SymbolTYPE(Symbol):
         self.final = self  # self.final always return the original aliased type (if this type is an alias)
         self.caseins = OPTIONS.case_insensitive.value  # Whether this ID is case insensitive or not
         self.class_ = CLASS.type_
+        self.accessed = False  # Whether this type has been used or not
 
     def __repr__(self):
         return "%s(%s)" % (self.token, str(self))
