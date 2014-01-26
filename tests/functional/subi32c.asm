5337842c750bf228154cf960f0cf4c2e26870a4f
Jose Rodriguez <boriel@users.noreply.github.com>
2014-01-26 10:09:15 +0000

    Test SUBI32 updated.

--- tests/functional/subi32c.asm
+++ tests/functional/subi32c.asm
@@ -128,6 +128,11 @@ __SWAP32:
 #line 60 "subi32c.bas"
 	
 ZXBASIC_USER_DATA:
+_level:
+	DEFB 01h
+	DEFB 00h
+	DEFB 00h
+	DEFB 00h
 _le:
 	DEFB 00h
 	DEFB 00h
@@ -135,11 +140,6 @@ _le:
 	DEFB 00h
 _l:
 	DEFB 00, 00, 00, 00
-_level:
-	DEFB 01h
-	DEFB 00h
-	DEFB 00h
-	DEFB 00h
 	; Defines DATA END --> HEAP size is 0
 ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
 	; Defines USER DATA Length in bytes
