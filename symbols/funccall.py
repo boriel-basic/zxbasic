7a6b5eaabbe2fc2af2378d2fc6d33601f703f233
boriel <boriel@localhost>
2014-01-26 10:02:26 +0000

    Better clarification comment.

--- symbols/funccall.py
+++ symbols/funccall.py
@@ -15,5 +15,14 @@ from call import SymbolCALL
 class SymbolFUNCCALL(SymbolCALL):
     ''' This class is the same as CALL, we just declare it to make
     a distinction. (e.g. the Token is gotten from the class name)
+
+    A FunctionCall is a Call whose return value is going to be used
+    later within an expression. Eg.
+
+    CALL: f(x)
+    FUNCCALL: a = 2 * f(x)
+
+    This distinction is useful to discard values returned using the HEAP
+    to avoid memory leaks.
     '''
     pass
