23dd42f04a67cc607555d2b45ef6866a4cf2c98d
Jose Rodriguez <boriel@users.noreply.github.com>
2014-02-05 19:21:06 +0000

    Added test FUNCCALL1 Test function call without parenthesis

---
+++ tests/functional/funccall1.asm
@@ -0,0 +1,58 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	call _test
+	push af
+	call _test2
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_test:
+	push ix
+	ld ix, 0
+	add ix, sp
+_test__leave:
+	ld sp, ix
+	pop ix
+	ret
+_test2:
+	push ix
+	ld ix, 0
+	add ix, sp
+_test2__leave:
+	ld sp, ix
+	pop ix
+	exx
+	pop hl
+	ex (sp), hl
+	exx
+	ret
+	
+ZXBASIC_USER_DATA:
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
---
+++ tests/functional/funccall1.bas
@@ -0,0 +1,13 @@
+REM checks for function call with no parenthesis
+
+Function test as Byte
+
+End Function
+
+Function test2(a As Byte)
+
+End function
+
+
+test2(test)
+
