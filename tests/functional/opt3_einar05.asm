b47d1ec50ce2c10bd90338d8e0f8e63ad9fcdd53
Jose Rodriguez <boriel@users.noreply.github.com>
2014-12-27 13:52:22 +0000

    Added test OPT_EINAR05 Test for I register and IM during O3 optimization.

---
+++ tests/functional/opt3_einar05.asm
@@ -0,0 +1,41 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+#line 0
+	call    $fc12
+	di
+	ld      a, $fe
+	ld      i, a
+	im      2
+	ei
+#line 6
+	ld bc, 0
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	pop iy
+	pop ix
+	exx
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	
+ZXBASIC_USER_DATA:
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
