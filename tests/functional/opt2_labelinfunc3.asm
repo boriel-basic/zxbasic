492b126a87778e20e2e51b8de2d4424ee9071e34
Jose Rodriguez <boriel@users.noreply.github.com>
2015-08-20 01:14:39 +0100

    Test added labelinfunc* Added test to check for labels in functions, accessed from outer scopes.

---
+++ tests/functional/opt2_labelinfunc3.asm
@@ -0,0 +1,46 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld a, 5
+	ld (__LABEL__label1), a
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	pop iy
+	pop ix
+	exx
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_procedure:
+	push ix
+	ld ix, 0
+	add ix, sp
+__LABEL__label1:
+_procedure__leave:
+	ld sp, ix
+	pop ix
+	ret
+	
+ZXBASIC_USER_DATA:
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
