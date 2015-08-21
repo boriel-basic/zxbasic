496946504f5535f25315a2cd1b91ba84146f4fcb
Jose Rodriguez <boriel@users.noreply.github.com>
2015-08-21 10:16:37 +0100

    Code clean up.

--- api/opcodestemps.py
+++ api/opcodestemps.py
@@ -19,33 +19,14 @@ singleton.count = 0
 
 
 class OpcodesTemps(object):
-    ''' Manages a table of Tn temporal values.
+    """ Manages a table of Tn temporal values.
         This should be a SINGLETON container
-    '''
-
+    """
     def __init__(self):
-        '''
-        self.table = {}
-        self.count = 0
-        '''
         self.data = singleton
 
-    '''
-    @classmethod
-    def __new__(clss, *args, **kwargs):
-        global __singleton
-
-        if __singleton is None:
-            __singleton = object.__new__(clss)
-
-        return clss._singleton
-    '''
-
     def new_t(self):
-        ''' Returns a new t-value name
-        '''
-        #self.count += 1
+        """ Returns a new t-value name
+        """
         self.data.count += 1
-
-        #return 't%i' % (self.count - 1)
         return 't%i' % (self.data.count - 1)
