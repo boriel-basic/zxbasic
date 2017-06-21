05daed3b1c9f1dcc32e17b5eeca8da193f0827b6
Jose Rodriguez <boriel@gmail.com>
2017-06-21 23:15:21 +0200

    test/update test for new constant expressions

--- tests/functional/optconst.asm
+++ tests/functional/optconst.asm
@@ -17,16 +17,16 @@ __START_PROGRAM:
 	ld (2 + __LABEL__label1), a
 	ld hl, __LABEL__label2 + __LABEL__label1 * 3
 	ld (2 + __LABEL__label2 * 5 - __LABEL__label1), hl
-	ld hl, __LABEL__label2 + __LABEL__label1 * 3
-	ld de, 0
+	ld hl, (__LABEL__label2 + __LABEL__label1 * 3 & 0xFFFFFFFF) & 0xFFFF
+	ld de, (__LABEL__label2 + __LABEL__label1 * 3 & 0xFFFFFFFF) >> 16
 	ld (2 + __LABEL__label2 * 5 - __LABEL__label1), hl
 	ld (2 + __LABEL__label2 * 5 - __LABEL__label1 + 2), de
-	ld hl, __LABEL__label2 + __LABEL__label1 * 3
-	ld de, 0
+	ld hl, (__LABEL__label2 + __LABEL__label1 * 3 & 0xFFFFFFFF) & 0xFFFF
+	ld de, (__LABEL__label2 + __LABEL__label1 * 3 & 0xFFFFFFFF) >> 16
 	ld (4), hl
 	ld (4 + 2), de
-	ld hl, __LABEL__label1 + __LABEL__label2
-	ld de, 0
+	ld hl, (__LABEL__label1 + __LABEL__label2 & 0xFFFFFFFF) & 0xFFFF
+	ld de, (__LABEL__label1 + __LABEL__label2 & 0xFFFFFFFF) >> 16
 	ld (_a), hl
 	ld (_a + 2), de
 	ld hl, 0
