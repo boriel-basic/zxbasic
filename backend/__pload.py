61cbc0d35c92d3c0679648b90c5ce0e7a5b10a4d
Bartolome Sanchez Salado <aliaselbarto@gmail.com>
2017-08-04 22:57:27 +0200

    Add flake8 checks in backend dir

--- backend/__pload.py
+++ backend/__pload.py
@@ -32,7 +32,7 @@ def _paddr(ins):
 
     I = int(oper)
     if I >= 0:
-        I += 4 # Return Address + "push IX"
+        I += 4  # Return Address + "push IX"
 
     output.append('push ix')
     output.append('pop hl')
@@ -47,7 +47,6 @@ def _paddr(ins):
 
     output.append('push hl')
     return output
-    
 
 
 def _pload(offset, size):
@@ -66,16 +65,16 @@ def _pload(offset, size):
         offset = offset[1:]
 
     I = int(offset)
-    if I >= 0: # If it is a parameter, round up to even bytes
-        I += 4 + (size % 2 if not indirect else 0) # Return Address + "push IX"
+    if I >= 0:  # If it is a parameter, round up to even bytes
+        I += 4 + (size % 2 if not indirect else 0)  # Return Address + "push IX"
 
-    ix_changed = (indirect or size < 5) and (abs(I) + size) > 127 # Offset > 127 bytes. Need to change IX
-    if ix_changed: # more than 1 byte
+    ix_changed = (indirect or size < 5) and (abs(I) + size) > 127  # Offset > 127 bytes. Need to change IX
+    if ix_changed:  # more than 1 byte
         output.append('push ix')
         output.append('ld de, %i' % I)
         output.append('add ix, de')
         I = 0
-    elif size == 5: # For floating point numbers we always use DE as IX offset
+    elif size == 5:  # For floating point numbers we always use DE as IX offset
         output.append('push ix')
         output.append('pop hl')
         output.append('ld de, %i' % I)
@@ -85,7 +84,7 @@ def _pload(offset, size):
     if indirect:
         output.append('ld h, (ix%+i)' % (I + 1))
         output.append('ld l, (ix%+i)' % I)
-        
+
         if size == 1:
             output.append('ld a, (hl)')
         elif size == 2:
@@ -96,30 +95,29 @@ def _pload(offset, size):
         elif size == 4:
             output.append('call __ILOAD32')
             REQUIRES.add('iload32.asm')
-        else: # Floating point
+        else:  # Floating point
             output.append('call __ILOADF')
             REQUIRES.add('iloadf.asm')
     else:
         if size == 1:
             output.append('ld a, (ix%+i)' % I)
         else:
-            if size <= 4: # 16/32bit integer, low part
+            if size <= 4:  # 16/32bit integer, low part
                 output.append('ld l, (ix%+i)' % I)
                 output.append('ld h, (ix%+i)' % (I + 1))
 
-                if size > 2: # 32 bit integer, high part
+                if size > 2:  # 32 bit integer, high part
                     output.append('ld e, (ix%+i)' % (I + 2))
                     output.append('ld d, (ix%+i)' % (I + 3))
 
-            else: # Floating point
+            else:  # Floating point
                 output.append('call __PLOADF')
                 REQUIRES.add('ploadf.asm')
-            
+
     if ix_changed:
         output.append('pop ix')
 
     return output
-    
 
 
 def _pload8(ins):
@@ -135,7 +133,6 @@ def _pload8(ins):
     return output
 
 
-
 def _pload16(ins):
     ''' Loads from stack pointer (SP) + X, being
     X 2st parameter.
@@ -146,7 +143,6 @@ def _pload16(ins):
     output = _pload(ins.quad[2], 2)
     output.append('push hl')
     return output
-
 
 
 def _pload32(ins):
@@ -160,7 +156,6 @@ def _pload32(ins):
     output.append('push de')
     output.append('push hl')
     return output
-    
 
 
 def _ploadf(ins):
@@ -215,39 +210,39 @@ def _pstore8(ins):
     value = ins.quad[2]
     offset = ins.quad[1]
     indirect = offset[0] == '*'
-    size = 0 
+    size = 0
     if indirect:
         offset = offset[1:]
         size = 1
 
     I = int(offset)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
         if not indirect:
-            I += 1 # F flag ignored
+            I += 1  # F flag ignored
 
     if is_int(value):
         output = []
     else:
         output = _8bit_oper(value)
 
-    ix_changed = not (-128 + size <= I <= 127 - size) # Offset > 127 bytes. Need to change IX
-    if ix_changed: # more than 1 byte
+    ix_changed = not (-128 + size <= I <= 127 - size)  # Offset > 127 bytes. Need to change IX
+    if ix_changed:  # more than 1 byte
         output.append('push ix')
         output.append('pop hl')
         output.append('ld de, %i' % I)
         output.append('add hl, de')
-        
+
     if indirect:
         if ix_changed:
-            output.append('ld c, (hl)') 
+            output.append('ld c, (hl)')
             output.append('inc hl')
             output.append('ld h, (hl)')
             output.append('ld l, c')
         else:
             output.append('ld h, (ix%+i)' % (I + 1))
             output.append('ld l, (ix%+i)' % I)
-        
+
         if is_int(value):
             output.append('ld (hl), %i' % int8(value))
         else:
@@ -272,7 +267,6 @@ def _pstore8(ins):
     return output
 
 
-
 def _pstore16(ins):
     ''' Stores 2nd parameter at stack pointer (SP) + X, being
     X 1st parameter.
@@ -282,21 +276,21 @@ def _pstore16(ins):
     value = ins.quad[2]
     offset = ins.quad[1]
     indirect = offset[0] == '*'
-    size = 1 
+    size = 1
     if indirect:
         offset = offset[1:]
 
     I = int(offset)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
 
     if is_int(value):
         output = []
     else:
         output = _16bit_oper(value)
 
-    ix_changed = not (-128 + size <= I <= 127 - size) # Offset > 127 bytes. Need to change IX
-        
+    ix_changed = not (-128 + size <= I <= 127 - size)  # Offset > 127 bytes. Need to change IX
+
     if indirect:
         if is_int(value):
             output.append('ld hl, %i' % int16(value))
@@ -307,7 +301,7 @@ def _pstore16(ins):
         return output
 
     # direct store
-    if ix_changed: # more than 1 byte
+    if ix_changed:  # more than 1 byte
         if not is_int(value):
             output.append('ex de, hl')
 
@@ -339,7 +333,6 @@ def _pstore16(ins):
     return output
 
 
-
 def _pstore32(ins):
     ''' Stores 2nd parameter at stack pointer (SP) + X, being
     X 1st parameter.
@@ -349,17 +342,15 @@ def _pstore32(ins):
     value = ins.quad[2]
     offset = ins.quad[1]
     indirect = offset[0] == '*'
-    bytes = 3 
     if indirect:
         offset = offset[1:]
 
     I = int(offset)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
 
     output = _32bit_oper(value)
-    ix_changed = not (-128 + bytes <= I <= 127 - bytes) # Offset > 127 bytes. Need to change IX
-        
+
     if indirect:
         output.append('ld bc, %i' % I)
         output.append('call __PISTORE32')
@@ -372,7 +363,6 @@ def _pstore32(ins):
     REQUIRES.add('pstore32.asm')
 
     return output
-    
 
 
 def _pstoref16(ins):
@@ -384,17 +374,15 @@ def _pstoref16(ins):
     value = ins.quad[2]
     offset = ins.quad[1]
     indirect = offset[0] == '*'
-    bytes = 3 
     if indirect:
         offset = offset[1:]
 
     I = int(offset)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
 
     output = _f16_oper(value)
-    ix_changed = not (-128 + bytes <= I <= 127 - bytes) # Offset > 127 bytes. Need to change IX
-        
+
     if indirect:
         output.append('ld bc, %i' % I)
         output.append('call __PISTORE32')
@@ -407,7 +395,6 @@ def _pstoref16(ins):
     REQUIRES.add('pstore32.asm')
 
     return output
-
 
 
 def _pstoref(ins):
@@ -424,10 +411,10 @@ def _pstoref(ins):
 
     I = int(offset)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
 
     output = _float_oper(value)
-        
+
     if indirect:
         output.append('ld hl, %i' % I)
         output.append('call __PISTOREF')
@@ -440,7 +427,6 @@ def _pstoref(ins):
     REQUIRES.add('pstoref.asm')
 
     return output
-
 
 
 def _pstorestr(ins):
@@ -489,7 +475,7 @@ def _pstorestr(ins):
 
     I = int(value)
     if I >= 0:
-        I += 4 # Return Address + "push IX" 
+        I += 4  # Return Address + "push IX"
 
     output.append('ld bc, %i' % I)
 
@@ -509,4 +495,3 @@ def _pstorestr(ins):
             REQUIRES.add('pstorestr2.asm')
 
     return output
-
