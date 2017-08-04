80422f22de400ad7cd4dfc4a87491cba2584bcb6
Jose Rodriguez <boriel@gmail.com>
2017-08-05 01:16:56 +0200

    bugfix: uses delayed import
    test.py stopped working without a PYTHONPATH.
    Uses delayed import and add ZXBasic Root directory
    to sys.path so it works again.

--- tests/functional/test.py
+++ tests/functional/test.py
@@ -7,7 +7,6 @@ import os
 import re
 import subprocess
 import difflib
-from api.utils import open_file
 
 BUFFSIZE = 1024
 CLOSE_STDERR = False
@@ -31,6 +30,7 @@ ZXBASM = os.path.join(ZXBASIC_ROOT, 'zxbasm.py')
 ZXBPP = os.path.join(ZXBASIC_ROOT, 'zxbpp.py')
 
 _original_root = "/src/zxb/trunk"
+sys.path.append(ZXBASIC_ROOT)
 
 
 def get_file_lines(filename, ignore_regexp=None, replace_regexp=None,
@@ -38,6 +38,7 @@ def get_file_lines(filename, ignore_regexp=None, replace_regexp=None,
     """ Opens source file <filename> and load its line,
     discarding those not important for comparison.
     """
+    from api.utils import open_file
     with open_file(filename, 'rt', 'utf-8') as f:
         lines = [x for x in f]
 
