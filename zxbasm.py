d653afb2c2cf5c4aa422ed3ffaa568d1d8e014ac
Jose Rodriguez <boriel@gmail.com>
2017-08-07 07:09:15 +0200

    drop optparse in favour of argparse in zxbasm
    Python's argparse is a newer and slightly enhanced options
    parsing library.

--- zxbasm.py
+++ zxbasm.py
@@ -13,7 +13,7 @@
 
 import sys
 import os
-from optparse import OptionParser
+import argparse
 
 import asmparse
 from asmparse import Asm, Expr, Container
@@ -25,57 +25,50 @@ from api.config import OPTIONS
 VERSION = '1.10'
 
 
-def main():
-    # Create default options
-    OPTIONS.add_option_if_not_defined('optimization', int, 0)
-
+def main(args=None):
     # Create option parser
-    o_parser = OptionParser(usage='Usage: %prog <input file> [options]',
-                            version='%prog ' + VERSION)
+    o_parser = argparse.ArgumentParser(prog='zxbasm')
+    o_parser.add_argument('PROGRAM', type=str, help='ASM program file')
+    o_parser.add_argument("-d", "--debug", action="count", default=OPTIONS.Debug.value,
+                          help="Enable verbosity/debugging output")
 
-    o_parser.add_option("-d", "--debug",
-                        action="count", dest="debug", default=OPTIONS.Debug.value,
-                        help="Enable verbosity/debugging output")
+    o_parser.add_argument("-O", "--optimize", type=int, dest="optimization_level",
+                          help="Sets optimization level. 0 = None", default=OPTIONS.optimization.value)
 
-    o_parser.add_option("-O", "--optimize", type="int", dest="optimization_level",
-                        help="Sets optimization level. 0 = None", default=OPTIONS.optimization.value)
+    o_parser.add_argument("-o", "--output", type=str, dest="output_file",
+                          help="Sets output file. Default is input filename with .bin extension", default=None)
 
-    o_parser.add_option("-o", "--output", type="string", dest="output_file",
-                        help="Sets output file. Default is input filename with .bin extension", default=None)
+    o_parser.add_argument("-T", "--tzx", action="store_true", dest="tzx", default=False,
+                          help="Sets output format to tzx (default is .bin)")
 
-    o_parser.add_option("-T", "--tzx", action="store_true", dest="tzx", default=False,
-                        help="Sets output format to tzx (default is .bin)")
+    o_parser.add_argument("-t", "--tap", action="store_true", dest="tap", default=False,
+                          help="Sets output format to tzx (default is .bin)")
 
-    o_parser.add_option("-t", "--tap", action="store_true", dest="tap", default=False,
-                        help="Sets output format to tzx (default is .bin)")
+    o_parser.add_argument("-B", "--BASIC", action="store_true", dest="basic", default=False,
+                          help="Creates a BASIC loader which load the rest of the CODE. Requires -T ot -t")
 
-    o_parser.add_option("-B", "--BASIC", action="store_true", dest="basic", default=False,
-                        help="Creates a BASIC loader which load the rest of the CODE. Requires -T ot -t")
+    o_parser.add_argument("-a", "--autorun", action="store_true", default=False,
+                          help="Sets the program to auto run once loaded (implies --BASIC)")
 
-    o_parser.add_option("-a", "--autorun", action="store_true", dest="autorun", default=False,
-                        help="Sets the program to auto run once loaded (implies --BASIC)")
+    o_parser.add_argument("-e", "--errmsg", type=str, dest="stderr", default=OPTIONS.StdErrFileName.value,
+                          help="Error messages file (standard error console by default")
 
-    o_parser.add_option("-e", "--errmsg", type="string", dest="stderr", default=OPTIONS.StdErrFileName.value,
-                        help="Error messages file (standard error console by default")
+    o_parser.add_argument("-M", "--mmap", type=str, dest="memory_map", default=None,
+                          help="Generate label memory map")
 
-    o_parser.add_option("-M", "--mmap", type="string", dest="memory_map", default=None,
-                        help="Generate label memory map")
+    o_parser.add_argument("-b", "--bracket", action="store_true", default=False,
+                          help="Allows brackets only for memory access and indirections")
 
-    o_parser.add_option("-b", "--bracket", action="store_true", dest="bracket", default=False,
-                        help="Allows brackets only for memory access and indirections")
+    o_parser.add_argument("--version", action="version", version="%(prog)s " + VERSION)
 
-    (options, args) = o_parser.parse_args()
+    options = o_parser.parse_args(args)
 
-    if len(args) != 1:
-        o_parser.error("missing input file. (Try -h)")
-        sys.exit(3)
-
-    if not os.path.exists(args[0]):
-        o_parser.error("No such file or directory: '%s'" % args[0])
+    if not os.path.exists(options.PROGRAM):
+        o_parser.error("No such file or directory: '%s'" % options.PROGRAM)
         sys.exit(2)
 
     OPTIONS.Debug.value = int(options.debug)
-    OPTIONS.inputFileName.value = args[0]
+    OPTIONS.inputFileName.value = options.PROGRAM
     OPTIONS.outputFileName.value = options.output_file
     OPTIONS.optimization.value = options.optimization_level
     OPTIONS.use_loader.value = options.autorun or options.basic
@@ -98,11 +91,11 @@ def main():
 
     if int(options.tzx) + int(options.tap) > 1:
         o_parser.error("Options --tap, --tzx and --asm are mutually exclusive")
-        sys.exit(3)
+        return 3
 
     if OPTIONS.use_loader.value and not options.tzx and not options.tap:
         o_parser.error('Option --BASIC and --autorun requires --tzx or tap format')
-        sys.exit(4)
+        return 4
 
     # Configure the preprocessor to use the asm-preprocessor-lexer
     zxbpp.setMode('asm')
@@ -115,7 +108,7 @@ def main():
     asmparse.assemble(asm_output)
     if not asmparse.MEMORY.memory_bytes:  # empty seq.
         asmparse.warning(0, "Nothing to assemble. Exiting...")
-        sys.exit(0)
+        return 0
 
     current_org = max(asmparse.MEMORY.memory_bytes.keys() or [0]) + 1
 
@@ -137,7 +130,8 @@ def main():
             f.write(asmparse.MEMORY.memory_map)
 
     asmparse.generate_binary(OPTIONS.outputFileName.value, OPTIONS.output_file_type.value)
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
