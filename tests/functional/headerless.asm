bff9474c3bf7159b16ca2caef6564a887934db43
Jose Rodriguez <boriel@gmail.com>
2017-10-18 22:59:06 +0200

    add headerless mode
    With the command line flag --headerless the compiler does not
    generate neither ASM prologue nor epilogue.

---
+++ tests/functional/headerless.asm
@@ -0,0 +1,17 @@
+	ld a, (_a)
+	inc a
+	ld (_a), a
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	ret
+
+ZXBASIC_USER_DATA:
+_a:
+	DEFB 02h
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
---
+++ tests/functional/headerless.bas
@@ -0,0 +1,6 @@
+REM a headerless (no prologuqe in ASM) program
+#pragma headerless=true
+
+DIM a as UByte = 2
+LET a = a + 1
+
