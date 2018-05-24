37c28585d5a4806b02d3d35e351aa6634fed3c9a
Jose Rodriguez <boriel@gmail.com>
2018-05-24 22:39:28 +0200

    test updated g*

--- tests/functional/geu32.asm
+++ tests/functional/geu32.asm
@@ -14,12 +14,8 @@ __START_PROGRAM:
 	ld de, (_level + 2)
 	push de
 	push hl
-	ld hl, (_le + 2)
-	push hl
+	ld de, (_le + 2)
 	ld hl, (_le)
-	push hl
-	pop hl
-	pop de
 	call __SWAP32
 	call __SUB32
 	ccf
@@ -135,7 +131,7 @@ __SUB32:
 		push bc		; puts return address back
 		exx
 		ret
-#line 98 "geu32.bas"
+#line 94 "geu32.bas"
 #line 1 "swap32.asm"
 
 	; Exchanges current DE HL with the
@@ -154,7 +150,7 @@ __SWAP32:
 	    push bc
 		ret
 
-#line 99 "geu32.bas"
+#line 95 "geu32.bas"
 
 ZXBASIC_USER_DATA:
 _level:
