f9c8cb75106d8db632a9bd0c697d5117ce8b4596
Jose Rodriguez <boriel@gmail.com>
2018-09-20 23:38:52 +0200

    bugfix: END sentence
    End sentence was not being emitted correctly. Fixed.

---
+++ tests/functional/opt1_endtest.asm
@@ -0,0 +1,42 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld hl, (_N)
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	ld hl, 0
+	ld b, h
+	ld c, l
+	jp __END_PROGRAM
+
+ZXBASIC_USER_DATA:
+_N:
+	DEFB 39h
+	DEFB 30h
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
