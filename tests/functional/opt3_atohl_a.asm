fd36ff0d7133e3a341edca076c1bbcfab0c7a9c5
Jose Rodriguez <boriel@gmail.com>
2019-05-25 00:18:56 +0200

    Add optimizer template 052
    ;; Replaces sequence:
    ;; ld hl, (_XXXX)
    ;; ld a, l
    ;;
    ;; With:
    ;; ld a, (_XXXX)

---
+++ tests/functional/opt3_atohl_a.asm
@@ -0,0 +1,48 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	ld a, (_g)
+	push af
+	ld a, (_i)
+	push af
+	ld hl, (_dw1)
+	call _putTile10x8Box
+	ld bc, 0
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	pop iy
+	pop ix
+	exx
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_putTile10x8Box:
+_putTile10x8Box__leave:
+	ret
+
+ZXBASIC_USER_DATA:
+_dw1:
+	DEFB 00, 00
+_i:
+	DEFB 00, 00
+_g:
+	DEFB 00, 00
+	; Defines DATA END --> HEAP size is 0
+ZXBASIC_USER_DATA_END EQU ZXBASIC_MEM_HEAP
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	END
