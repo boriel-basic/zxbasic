fe1463bb5070da39165338d35f1b0a87c8f451bc
Jose Rodriguez <boriel@gmail.com>
2019-05-30 23:58:30 +0200

    Add function simplify_asm_arg
    Simplifies asm instructions like:
    
     ld a, (1235 + 4)
    
    becomes:
    
     ld a, (1239)
    
    Also refacts is_number to use is_mem_access().

--- tests/arch/zx48k/optimizer/test_optimizer_asm.py
+++ tests/arch/zx48k/optimizer/test_optimizer_asm.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from arch.zx48k.optimizer import asm
+from arch.zx48k.optimizer import asm, helpers
 
 
 class TestASM(unittest.TestCase):
@@ -17,3 +17,16 @@ class TestASM(unittest.TestCase):
     def test_unknown_instruction(self):
         a = asm.Asm(' unknown instr ')
         self.assertEqual(a.bytes, ())
+
+    def test_simplify_arg(self):
+        a = helpers.simplify_asm_args('ld a, (126 - 1)')
+        self.assertEqual('ld a, (125)', a)
+
+        a = helpers.simplify_asm_args('ld hl, (30 + 40)')
+        self.assertEqual('ld hl, (70)', a)
+
+        a = helpers.simplify_asm_args('ld hl, ((30) + (40))')
+        self.assertEqual('ld hl, (70)', a)
+
+        a = helpers.simplify_asm_args('ld de, (30) + (40)')
+        self.assertEqual('ld de, (70)', a)
