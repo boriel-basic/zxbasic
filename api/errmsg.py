c239b1885c30943a172ce9450cbeff647221ff0c
Jose Rodriguez <boriel@gmail.com>
2019-06-25 23:29:57 +0200

    Fix a typo in function name

--- api/errmsg.py
+++ api/errmsg.py
@@ -185,5 +185,5 @@ def syntax_error_undeclared_type(lineno, id_):
 # ----------------------------------------
 #  Cannot assign a value to 'var'. It's not a variable
 # ----------------------------------------
-def syntax_error_cannot_assing_not_a_var(lineno, id_):
+def syntax_error_cannot_assign_not_a_var(lineno, id_):
     syntax_error(lineno, "Cannot assign a value to '%s'. It's not a variable" % id_)
