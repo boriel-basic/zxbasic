d1e0fa07c65f2ec5069855684b7dea029e19f167
Jose Rodriguez <boriel@gmail.com>
2020-05-24 00:11:39 +0200

    Use poetry for packaging
    Seems to be much easier.

--- setup.py
+++ setup.py
@@ -1,70 +1,47 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-from distutils.core import setup
-from setuptools import find_packages
-from collections import defaultdict
+from setuptools import setup
 
-from zxb import version
+packages = \
+    ['api',
+     'arch',
+     'arch.zx48k',
+     'arch.zx48k.backend',
+     'arch.zx48k.optimizer',
+     'arch.zx48k.peephole',
+     'ast_',
+     'outfmt',
+     'parsetab',
+     'ply',
+     'symbols',
+     'zxb',
+     'zxbasm',
+     'zxbpp',
+     'zxbpp.prepro']
 
+package_data = \
+    {'': ['*'], 'arch.zx48k.peephole': ['opts/*']}
 
-def get_files(folders):
-    result = defaultdict(list)
+entry_points = \
+    {'console_scripts': ['zxb = zxb.zxb:main',
+                         'zxbasm = zxbasm.zxbasm:main',
+                         'zxbpp = zxbpp.zxbpp:entry_point']}
 
-    for folder in folders:
-        for dir_, subdirs, files_ in os.walk(folder):
-            for fpath in [os.path.join(dir_, f) for f in files_]:
-                result[os.path.dirname(fpath)].append(fpath)
+setup_kwargs = {
+    'name': 'zxbasic',
+    'version': '1.10.0',
+    'description': "Boriel's ZX BASIC Compiler",
+    'long_description': None,
+    'author': 'Jose Rodriguez',
+    'author_email': 'boriel@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
 
-    return result
-
-
-file_dirs = 'library', 'library-asm'
-data_files = [(os.path.join('bin', dir_), fpath) for dir_, fpath in get_files(file_dirs).items()]
-
-setup(
-    name='zxbasic',
-    scripts=[
-        'bin/zxbc',
-        'bin/zxb',  # deprecated
-        'bin/zxbasm',
-        'bin/zxbpp'
-    ],
-    py_modules=['asm', 'asmlex', 'asmparse', 'keywords', 'optimizer', 'version', 'identityset',
-                'parsetab', 'zxbpptab', 'zxbasmtab', 'basic',
-                'z80', 'zxblex', 'zxbparser', 'zxbpplex', 'zxbasmpplex'],
-    packages=find_packages(exclude='test'),
-    version=version.VERSION,
-    description='The ZX Basic compiler',
-    long_description="A BASIC to Z80 cpu asm / machine code compiler.\n"
-                     "It mostly targets ZX Spectrum vintage machine but can be\n"
-                     "used for other purposes.",
-    author='Jose Rodriguez',
-    author_email='boriel@gmail.com',
-    url='https://bitbucket.org/boriel/zxbasic',
-    download_url='http://boriel.com/files/zxb/zxbasic-%s.tar.gz' % version.VERSION,
-    keywords=['compiler', 'zxspectrum', 'BASIC', 'z80'],  # arbitrary keywords
-    data_files=data_files,
-    license='GPL3',
-    classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 5 - Production/Stable',
-
-        # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    tags=['BASIC', 'zxspectrum', 'compiler', 'z80']
-)
+setup(**setup_kwargs)
