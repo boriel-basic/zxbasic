d12fa2cb5de4ee423e7e16c76f968de22c8167b7
Jose Rodriguez <boriel@gmail.com>
2020-10-22 00:58:40 +0200

    Fix bug when passing parameters
    Parameters of type Byte / Ubyte has an odd alignment.
    When passing the address of such parameters in a function
    call as a ByRef, the offset must be increased by one.

---
+++ tests/functional/byrefbyref.asm
@@ -0,0 +1,73 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	jp __MAIN_PROGRAM__
+ZXBASIC_USER_DATA:
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	.__LABEL__.ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_LEN
+	.__LABEL__.ZXBASIC_USER_DATA EQU ZXBASIC_USER_DATA
+_v:
+	DEFB 00
+ZXBASIC_USER_DATA_END:
+__MAIN_PROGRAM__:
+	ld hl, _v
+	push hl
+	call _primero
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+_primero:
+	push ix
+	ld ix, 0
+	add ix, sp
+	ld l, (ix+4)
+	ld h, (ix+5)
+	push hl
+	call _segundo
+_primero__leave:
+	ld sp, ix
+	pop ix
+	exx
+	pop hl
+	ex (sp), hl
+	exx
+	ret
+_segundo:
+	push ix
+	ld ix, 0
+	add ix, sp
+	ld h, (ix+5)
+	ld l, (ix+4)
+	ld (hl), 2
+_segundo__leave:
+	ld sp, ix
+	pop ix
+	exx
+	pop hl
+	ex (sp), hl
+	exx
+	ret
+	END
