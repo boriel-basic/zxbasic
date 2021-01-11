93cb5533eb9505595720b7e282d4da9d013637b6
Jose Rodriguez <boriel@gmail.com>
2021-01-12 00:04:14 +0100

    Fix wrong line number within asm context

---
+++ tests/functional/asm_error_line.asm
@@ -0,0 +1,44 @@
+	org 32768
+__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld hl, 0
+	add hl, sp
+	ld (__CALL_BACK__), hl
+	ei
+	jp __MAIN_PROGRAM__
+ZXBASIC_USER_DATA:
+	; Defines USER DATA Length in bytes
+ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_END - ZXBASIC_USER_DATA
+	.__LABEL__.ZXBASIC_USER_DATA_LEN EQU ZXBASIC_USER_DATA_LEN
+	.__LABEL__.ZXBASIC_USER_DATA EQU ZXBASIC_USER_DATA
+ZXBASIC_USER_DATA_END:
+__MAIN_PROGRAM__:
+#line 2 "asm_error_line.bas"
+test: dk 10, 20
+#line 6 "asm_error_line.bas"
+#line 6 "asm_error_line.bas"
+		dk 10, 20
+#line 10 "asm_error_line.bas"
+	ld hl, 0
+	ld b, h
+	ld c, l
+__END_PROGRAM:
+	di
+	ld hl, (__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+__CALL_BACK__:
+	DEFW 0
+	;; --- end of user code ---
+	END
