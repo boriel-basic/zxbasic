409e96617ab0e3968ac1fab13f13d34714c9b3df
Jose Rodriguez <boriel@gmail.com>
2021-08-22 12:34:05 +0200

    refact: add typing to decorator

--- src/api/decorator.py
+++ src/api/decorator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from typing import Callable
+
 
 class classproperty:
     """ Decorator for class properties.
     Use @classproperty instead of @property to add properties
     to the class object.
     """
-    def __init__(self, fget):
+    def __init__(self, fget: Callable):
         self.fget = fget
 
     def __get__(self, owner_self, owner_cls):
