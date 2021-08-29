30b3eb26487c2bbfa5f1d19558b0115ee4d80de9
Jose Rodriguez <boriel@gmail.com>
2021-08-29 18:15:29 +0200

    refact: move Label and Expr classes to their on modules
    Also creates a global_ module for zxbasm within it.
    Also fix some comments.

--- src/zxbasm/asmparse.py
+++ src/zxbasm/asmparse.py
@@ -13,39 +13,32 @@
 
 import os
 
-from typing import Any
-from typing import Dict
-from typing import List
 from typing import Optional
-from typing import Tuple
-from typing import NamedTuple
 
 import src.ply.yacc as yacc
 import src.api.utils
 from src.zxbasm import global_ as asm_gl
 
 from src import outfmt
-from src.api import errmsg
+from src.api import global_ as gl
 
 from src.api.debug import __DEBUG__
 from src.api.config import OPTIONS
 from src.api.errmsg import error
-from src.api.errmsg import warning
-from src.api import global_ as gl
 from src.zxbpp import zxbpp
 
 from src.zxbasm import asmlex, basic
 from src.zxbasm.asmlex import tokens  # noqa
 from src.zxbasm.global_ import DOT
-from src.zxbasm.asm import AsmInstruction, Error
 from src.zxbasm.expr import Expr
-from src.zxbasm.label import Label
+from src.zxbasm.asm import Container, Asm
+from src.zxbasm.memory import Memory
 
 LEXER = asmlex.Lexer()
 
 ORG = 0  # Origin of CODE
 INITS = []
-MEMORY: Optional['Memory'] = None  # Memory for instructions (Will be initialized with a Memory() instance)
+MEMORY: Optional[Memory] = None  # Memory for instructions (Will be initialized with a Memory() instance)
 AUTORUN_ADDR = None  # Where to start the execution automatically
 
 REGS16 = {'BC', 'DE', 'HL', 'SP', 'IX', 'IY'}  # 16 Bits registers
@@ -57,20 +50,6 @@ precedence = (
     ('right', 'POW'),
     ('right', 'UMINUS'),
 )
-
-MAX_MEM = 65535  # Max memory limit
-
-
-def normalize_namespace(namespace: str) -> str:
-    """ Given a namespace (e.g. '.' or 'mynamespace' or '..a...b....c'),
-    returns it in normalized form. That is:
-        - always prefixed with a dot
-        - no trailing dots
-        - any double dots are converted to single dot (..my..namespace => .my.namespace)
-        - one or more dots (e.g. '.', '..', '...') are converted to '.' (Global namespace)
-    """
-    namespace = (DOT + DOT.join(x for x in namespace.split(DOT) if x))
-    return namespace
 
 
 def init():
@@ -91,363 +70,6 @@ def init():
 
     # Current namespace (defaults to ''). It's a prefix added to each global label
     asm_gl.NAMESPACE = asm_gl.GLOBAL_NAMESPACE
-
-
-class Asm(AsmInstruction):
-    """ Class extension to AsmInstruction with a short name :-P
-    and will trap some exceptions and convert them to error msgs.
-
-    It will also record source line
-    """
-
-    def __init__(self, lineno, asm, arg=None):
-        self.lineno = lineno
-
-        if asm not in ('DEFB', 'DEFS', 'DEFW'):
-            try:
-                super(Asm, self).__init__(asm, arg)
-            except Error as v:
-                error(lineno, v.msg)
-                return
-
-            self.pending = len([x for x in self.arg if isinstance(x, Expr) and x.try_eval() is None]) > 0
-
-            if not self.pending:
-                self.arg = self.argval()
-        else:
-            self.asm = asm
-            self.pending = True
-
-            if isinstance(arg, str):
-                self.arg = tuple([Expr(Container(ord(x), lineno)) for x in arg])
-            else:
-                self.arg = arg
-
-            self.arg_num = len(self.arg)
-
-    def bytes(self) -> bytearray:
-        """ Returns opcodes
-        """
-        if self.asm not in ('DEFB', 'DEFS', 'DEFW'):
-            if self.pending:
-                tmp = self.arg  # Saves current arg temporarily
-                self.arg = (0, ) * self.arg_num
-                result = super(Asm, self).bytes()
-                self.arg = tmp  # And recovers it
-
-                return result
-
-            return super(Asm, self).bytes()
-
-        if self.asm == 'DEFB':
-            if self.pending:
-                return bytearray((0, ) * self.arg_num)
-
-            return bytearray(x & 0xFF for x in self.argval())
-
-        if self.asm == 'DEFS':
-            if self.pending:
-                N = self.arg[0]
-                if isinstance(N, Expr):
-                    N = N.eval()
-                return (0, ) * N
-
-            args = self.argval()
-            arg0 = args[0]
-            arg1 = args[1]
-            assert isinstance(arg0, int)
-            assert isinstance(arg1, int)
-
-            if arg1 > 255:
-                errmsg.warning_value_will_be_truncated(self.lineno)
-            num = arg1 & 0xFF
-            return bytearray((num, ) * arg0)
-
-        if self.pending:  # DEFW
-            return bytearray((0, ) * 2 * self.arg_num)
-
-        result = bytearray()
-        for i in self.argval():
-            x = i & 0xFFFF
-            result.extend([x & 0xFF, x >> 8])
-
-        return bytearray(result)
-
-    def argval(self):
-        """ Solve args values or raise errors if not
-        defined yet
-        """
-        if gl.has_errors:
-            return [None]
-
-        if self.asm in ('DEFB', 'DEFS', 'DEFW'):
-            result = tuple([x.eval() if isinstance(x, Expr) else x for x in self.arg])
-            if self.asm == 'DEFB' and any(x > 255 for x in result):
-                errmsg.warning_value_will_be_truncated(self.lineno)
-            return result
-
-        self.arg = tuple([x if not isinstance(x, Expr) else x.eval() for x in self.arg])
-        if gl.has_errors:
-            return [None]
-
-        if self.asm.split(' ')[0] in ('JR', 'DJNZ'):  # A relative jump?
-            if self.arg[0] < -128 or self.arg[0] > 127:
-                error(self.lineno, 'Relative jump out of range')
-                return [None]
-
-        return super(Asm, self).argval()
-
-
-class Container(NamedTuple):
-    """ Single class container
-    """
-    item: Any
-    lineno: int
-
-
-class Memory:
-    """ A class to describe memory
-    """
-
-    def __init__(self, org: int = 0):
-        """ Initializes the origin of code.
-        0 by default """
-        self.index = org  # ORG address (can be changed on the fly)
-        self.memory_bytes: Dict[int, int] = {}  # An array (associative) containing memory bytes
-        self.local_labels: List[Dict[str, Label]] = [{}]  # Local labels in the current memory scope
-        self.global_labels = self.local_labels[0]  # Global memory labels
-        self.tmp_labels: Dict[int, Label]
-        self.tmp_labels_lines: List = []
-        self.ORG = org  # last ORG value set
-        self.scopes: List[int] = []
-
-        # Origins of code for asm mnemonics.
-        # This will store corresponding asm instructions
-        self.orgs: Dict[int, List[Asm]] = {}
-
-    def enter_proc(self, lineno: int):
-        """ Enters (pushes) a new context
-        """
-        self.local_labels.append({})  # Add a new context
-        self.scopes.append(lineno)
-        __DEBUG__('Entering scope level %i at line %i' % (len(self.scopes), lineno))
-
-    def set_org(self, value: int, lineno: int):
-        """ Sets a new ORG value
-        """
-        if value < 0 or value > MAX_MEM:
-            error(lineno, "Memory ORG out of range [0 .. 65535]. Current value: %i" % value)
-
-        self.index = self.ORG = value
-
-    @staticmethod
-    def id_name(label: str, namespace: Optional[str] = None) -> Tuple[str, str]:
-        """ Given a name and a namespace, resolves
-        returns the name as namespace + '.' + name. If namespace
-        is none, the current NAMESPACE is used
-        """
-        if namespace is None:
-            namespace = asm_gl.NAMESPACE
-
-        if label.isdecimal():  # temporary labels are just integer numbers
-            return label, namespace
-
-        if not label.startswith(DOT):
-            ex_label = normalize_namespace(f"{namespace}{DOT}{label}")  # The mangled namespace.labelname label
-            return ex_label, namespace
-
-        return label, namespace
-
-    @property
-    def org(self) -> int:
-        """ Returns current ORG index
-        """
-        return self.index
-
-    def __set_byte(self, byte: int, lineno: int):
-        """ Sets a byte at the current location,
-        and increments org in one. Raises an error if org > MAX_MEMORY
-        """
-        if byte < 0 or byte > 255:
-            error(lineno, 'Invalid byte value %i' % byte)
-
-        self.memory_bytes[self.org] = byte
-        self.index += 1  # Increment current memory pointer
-
-    def exit_proc(self, lineno: int):
-        """ Exits current procedure. Local labels are transferred to global
-        scope unless they have been marked as local ones.
-
-        Raises an error if no current local context (stack underflow)
-        """
-        __DEBUG__('Exiting current scope from lineno %i' % lineno)
-
-        if len(self.local_labels) <= 1:
-            error(lineno, 'ENDP in global scope (with no PROC)')
-            return
-
-        for label in self.local_labels[-1].values():
-            if label.local:
-                if not label.defined:
-                    error(lineno, "Undefined LOCAL label '%s'" % label.name)
-                    return
-                continue
-
-            name = label.name
-            _lineno = label.lineno
-            value = label.value
-
-            if name not in self.global_labels.keys():
-                self.global_labels[name] = label
-            else:
-                self.global_labels[name].define(value, _lineno)
-
-        self.local_labels.pop()  # Removes current context
-        self.scopes.pop()
-
-    def set_memory_slot(self):
-        if self.org not in self.orgs:
-            self.orgs[self.org] = []  # Declares an empty memory slot if not already done
-            self.memory_bytes[self.org] = 0  # Declares an empty memory slot if not already done
-
-    def add_instruction(self, instr: Asm):
-        """ This will insert an asm instruction at the current memory position
-        in a t-uple as (mnemonic, params).
-
-        It will also insert the opcodes at the memory_bytes
-        """
-        if gl.has_errors:
-            return
-
-        __DEBUG__('%04Xh [%04Xh] ASM: %s' % (self.org, self.org - self.ORG, instr.asm))
-        self.set_memory_slot()
-        self.orgs[self.org].append(instr)
-
-        for byte in instr.bytes():
-            self.__set_byte(byte, instr.lineno)
-
-    def dump(self):
-        """ Returns a tuple containing code ORG (origin address), and a list of bytes (OUTPUT)
-        """
-        org = min(self.memory_bytes.keys())  # Org is the lowest one
-        OUTPUT = []
-        align = []
-
-        for label in self.global_labels.values():
-            if not label.defined:
-                error(label.lineno, "Undefined GLOBAL label '%s'" % label.name)
-
-        for i in range(org, max(self.memory_bytes.keys()) + 1):
-            if gl.has_errors:
-                return org, OUTPUT
-
-            try:
-                try:
-                    a = [x for x in self.orgs[i] if isinstance(x, Asm)]  # search for asm instructions
-
-                    if not a:
-                        align.append(0)  # Fill with ZEROes not used memory regions
-                        continue
-
-                    OUTPUT += align
-                    align = []
-                    a = a[0]
-                    if a.pending:
-                        a.arg = a.argval()
-                        a.pending = False
-                        tmp = a.bytes()
-
-                        for r in range(len(tmp)):
-                            self.memory_bytes[i + r] = tmp[r]
-                except KeyError:
-                    pass
-
-                OUTPUT.append(self.memory_bytes[i])
-
-            except KeyError:
-                OUTPUT.append(0)  # Fill with ZEROes not used memory regions
-
-        return org, OUTPUT
-
-    def declare_label(
-            self,
-            label: str,
-            lineno: int,
-            value: int = None,
-            local: bool = False,
-            namespace: Optional[str] = None
-    ):
-        """ Sets a label with the given value or with the current address (org)
-        if no value is passed.
-
-        Exits with error if label already set, otherwise return the label object
-        """
-        ex_label, namespace = Memory.id_name(label, namespace)
-
-        is_address = value is None
-        if value is None:
-            value = self.org
-
-        if is_address:
-            __DEBUG__(f"Declaring '{ex_label}' (value {'%04Xh' % value}) in {lineno}")
-        else:
-            __DEBUG__(f"Declaring '{ex_label}' in {lineno}")
-
-        if ex_label in self.local_labels[-1].keys():
-            self.local_labels[-1][ex_label].define(value, lineno)
-            self.local_labels[-1][ex_label].is_address = is_address
-        else:
-            self.local_labels[-1][ex_label] = Label(ex_label, lineno, value, local, namespace, is_address)
-
-        self.set_memory_slot()
-
-        return self.local_labels[-1][ex_label]
-
-    def get_label(self, label: str, lineno: int) -> Label:
-        """ Returns a label in the current context or in the global one.
-        If the label does not exists, creates a new one and returns it.
-        """
-
-        ex_label, namespace = Memory.id_name(label)
-
-        for local_label in self.local_labels[::-1]:
-            result = local_label.get(ex_label)
-            if result is not None:
-                return result
-
-        result = Label(ex_label, lineno, namespace=namespace)
-        self.local_labels[-1][ex_label] = result  # HINT: no namespace
-
-        return result
-
-    def set_label(self, label: str, lineno: int, local: bool = False) -> Label:
-        """ Sets a label, lineno and local flag in the current scope
-        (even if it exist in previous scopes). If the label exist in
-        the current scope, changes it flags.
-
-        The resulting label is returned.
-        """
-        ex_label, namespace = Memory.id_name(label)
-
-        if ex_label in self.local_labels[-1].keys():
-            result = self.local_labels[-1][ex_label]
-            result.lineno = lineno
-        else:
-            result = self.local_labels[-1][ex_label] = Label(ex_label, lineno, namespace=asm_gl.NAMESPACE)
-
-        if result.local == local:
-            warning(lineno, "label '%s' already declared as LOCAL" % label)
-
-        result.local = local
-
-        return result
-
-    @property
-    def memory_map(self) -> str:
-        """ Returns a (very long) string containing a memory map
-            hex address: label
-        """
-        return '\n'.join(sorted("%04X: %s" % (x.value, x.name) for x in self.global_labels.values() if x.is_address))
 
 
 # -------- GRAMMAR RULES for the preprocessor ---------
@@ -739,7 +361,7 @@ def p_namespace(p):
     """ asm : NAMESPACE ID
     """
 
-    asm_gl.NAMESPACE = normalize_namespace(p[2])
+    asm_gl.NAMESPACE = asm_gl.normalize_namespace(p[2])
     __DEBUG__('Setting namespace to ' + (asm_gl.NAMESPACE or DOT), level=1)
 
 
@@ -749,7 +371,7 @@ def p_push_namespace(p):
     """
 
     asm_gl.NAMESPACE_STACK.append(asm_gl.NAMESPACE)
-    asm_gl.NAMESPACE = normalize_namespace(p[3] if len(p) == 4 else asm_gl.NAMESPACE)
+    asm_gl.NAMESPACE = asm_gl.normalize_namespace(p[3] if len(p) == 4 else asm_gl.NAMESPACE)
 
     if asm_gl.NAMESPACE != asm_gl.NAMESPACE_STACK[-1]:
         __DEBUG__('Setting namespace to ' + (asm_gl.NAMESPACE or DOT), level=1)
---
+++ src/zxbasm/memory.py
@@ -0,0 +1,255 @@
+from typing import Dict, List, Optional, Tuple
+
+from src.api import global_ as gl
+from src.zxbasm import global_ as asm_gl
+
+from src.api.errmsg import error, warning
+from src.api.debug import __DEBUG__
+
+from src.zxbasm.label import Label
+from src.zxbasm.asm import Asm
+from src.zxbasm.global_ import DOT
+
+
+class Memory:
+    """ A class to describe memory
+    """
+    MAX_MEM = 65535  # Max memory limit
+
+    def __init__(self, org: int = 0):
+        """ Initializes the origin of code.
+        0 by default """
+        self.index = org  # ORG address (can be changed on the fly)
+        self.memory_bytes: Dict[int, int] = {}  # An array (associative) containing memory bytes
+        self.local_labels: List[Dict[str, Label]] = [{}]  # Local labels in the current memory scope
+        self.global_labels = self.local_labels[0]  # Global memory labels
+        self.ORG = org  # last ORG value set
+        self.scopes: List[int] = []
+
+        # Origins of code for asm mnemonics.
+        # This will store corresponding asm instructions
+        self.orgs: Dict[int, List[Asm]] = {}
+
+    def enter_proc(self, lineno: int):
+        """ Enters (pushes) a new context
+        """
+        self.local_labels.append({})  # Add a new context
+        self.scopes.append(lineno)
+        __DEBUG__('Entering scope level %i at line %i' % (len(self.scopes), lineno))
+
+    def set_org(self, value: int, lineno: int):
+        """ Sets a new ORG value
+        """
+        if value < 0 or value > self.MAX_MEM:
+            error(lineno, "Memory ORG out of range [0 .. 65535]. Current value: %i" % value)
+
+        self.index = self.ORG = value
+
+    @staticmethod
+    def id_name(label: str, namespace: Optional[str] = None) -> Tuple[str, str]:
+        """ Given a name and a namespace, resolves
+        returns the name as namespace + '.' + name. If namespace
+        is none, the current NAMESPACE is used
+        """
+        if namespace is None:
+            namespace = asm_gl.NAMESPACE
+
+        if label.isdecimal():  # temporary labels are just integer numbers
+            return label, namespace
+
+        if not label.startswith(DOT):
+            ex_label = asm_gl.normalize_namespace(f"{namespace}{DOT}{label}")  # The mangled namespace.labelname label
+            return ex_label, namespace
+
+        return label, namespace
+
+    @property
+    def org(self) -> int:
+        """ Returns current ORG index
+        """
+        return self.index
+
+    def __set_byte(self, byte: int, lineno: int):
+        """ Sets a byte at the current location,
+        and increments org in one. Raises an error if org > MAX_MEMORY
+        """
+        if byte < 0 or byte > 255:
+            error(lineno, 'Invalid byte value %i' % byte)
+
+        self.memory_bytes[self.org] = byte
+        self.index += 1  # Increment current memory pointer
+
+    def exit_proc(self, lineno: int):
+        """ Exits current procedure. Local labels are transferred to global
+        scope unless they have been marked as local ones.
+
+        Raises an error if no current local context (stack underflow)
+        """
+        __DEBUG__('Exiting current scope from lineno %i' % lineno)
+
+        if len(self.local_labels) <= 1:
+            error(lineno, 'ENDP in global scope (with no PROC)')
+            return
+
+        for label in self.local_labels[-1].values():
+            if label.local:
+                if not label.defined:
+                    error(lineno, "Undefined LOCAL label '%s'" % label.name)
+                    return
+                continue
+
+            name = label.name
+            _lineno = label.lineno
+            value = label.value
+
+            if name not in self.global_labels.keys():
+                self.global_labels[name] = label
+            else:
+                self.global_labels[name].define(value, _lineno)
+
+        self.local_labels.pop()  # Removes current context
+        self.scopes.pop()
+
+    def set_memory_slot(self):
+        if self.org not in self.orgs:
+            self.orgs[self.org] = []  # Declares an empty memory slot if not already done
+            self.memory_bytes[self.org] = 0  # Declares an empty memory slot if not already done
+
+    def add_instruction(self, instr: Asm):
+        """ This will insert an asm instruction at the current memory position
+        in a t-uple as (mnemonic, params).
+
+        It will also insert the opcodes at the memory_bytes
+        """
+        if gl.has_errors:
+            return
+
+        __DEBUG__('%04Xh [%04Xh] ASM: %s' % (self.org, self.org - self.ORG, instr.asm))
+        self.set_memory_slot()
+        self.orgs[self.org].append(instr)
+
+        for byte in instr.bytes():
+            self.__set_byte(byte, instr.lineno)
+
+    def dump(self):
+        """ Returns a tuple containing code ORG (origin address), and a list of bytes (OUTPUT)
+        """
+        org = min(self.memory_bytes.keys())  # Org is the lowest one
+        OUTPUT = []
+        align = []
+
+        for label in self.global_labels.values():
+            if not label.defined:
+                error(label.lineno, "Undefined GLOBAL label '%s'" % label.name)
+
+        for i in range(org, max(self.memory_bytes.keys()) + 1):
+            if gl.has_errors:
+                return org, OUTPUT
+
+            try:
+                try:
+                    a = [x for x in self.orgs[i] if isinstance(x, Asm)]  # search for asm instructions
+
+                    if not a:
+                        align.append(0)  # Fill with ZEROes not used memory regions
+                        continue
+
+                    OUTPUT += align
+                    align = []
+                    a = a[0]
+                    if a.pending:
+                        a.arg = a.argval()
+                        a.pending = False
+                        tmp = a.bytes()
+
+                        for r in range(len(tmp)):
+                            self.memory_bytes[i + r] = tmp[r]
+                except KeyError:
+                    pass
+
+                OUTPUT.append(self.memory_bytes[i])
+
+            except KeyError:
+                OUTPUT.append(0)  # Fill with ZEROes not used memory regions
+
+        return org, OUTPUT
+
+    def declare_label(
+            self,
+            label: str,
+            lineno: int,
+            value: int = None,
+            local: bool = False,
+            namespace: Optional[str] = None
+    ):
+        """ Sets a label with the given value or with the current address (org)
+        if no value is passed.
+
+        Exits with error if label already set, otherwise return the label object
+        """
+        ex_label, namespace = Memory.id_name(label, namespace)
+
+        is_address = value is None
+        if value is None:
+            value = self.org
+
+        if is_address:
+            __DEBUG__(f"Declaring '{ex_label}' (value {'%04Xh' % value}) in {lineno}")
+        else:
+            __DEBUG__(f"Declaring '{ex_label}' in {lineno}")
+
+        if ex_label in self.local_labels[-1].keys():
+            self.local_labels[-1][ex_label].define(value, lineno)
+            self.local_labels[-1][ex_label].is_address = is_address
+        else:
+            self.local_labels[-1][ex_label] = Label(ex_label, lineno, value, local, namespace, is_address)
+
+        self.set_memory_slot()
+
+        return self.local_labels[-1][ex_label]
+
+    def get_label(self, label: str, lineno: int) -> Label:
+        """ Returns a label in the current context or in the global one.
+        If the label does not exists, creates a new one and returns it.
+        """
+
+        ex_label, namespace = Memory.id_name(label)
+
+        for local_label in self.local_labels[::-1]:
+            result = local_label.get(ex_label)
+            if result is not None:
+                return result
+
+        result = Label(ex_label, lineno, namespace=namespace)
+        self.local_labels[-1][ex_label] = result  # HINT: no namespace
+
+        return result
+
+    def set_label(self, label: str, lineno: int, local: bool = False) -> Label:
+        """ Sets a label, lineno and local flag in the current scope
+        (even if it exist in previous scopes). If the label exist in
+        the current scope, changes it flags.
+
+        The resulting label is returned.
+        """
+        ex_label, namespace = Memory.id_name(label)
+
+        if ex_label in self.local_labels[-1].keys():
+            result = self.local_labels[-1][ex_label]
+            result.lineno = lineno
+        else:
+            result = self.local_labels[-1][ex_label] = Label(ex_label, lineno, namespace=asm_gl.NAMESPACE)
+
+        if result.local == local:
+            warning(lineno, "label '%s' already declared as LOCAL" % label)
+
+        result.local = local
+
+        return result
+
+    @property
+    def memory_map(self) -> str:
+        """ Returns a (very long) string containing a memory map
+            hex address: label
+        """
+        return '\n'.join(sorted("%04X: %s" % (x.value, x.name) for x in self.global_labels.values() if x.is_address))
