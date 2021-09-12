5997741cf64b96af19f5e7183b19a8a0da515663
Jose Rodriguez <boriel@gmail.com>
2021-09-12 21:46:01 +0200

    feat: add MUL8 and MUL16 for zxnext
    Slightly faster and shorter.

--- src/arch/zxnext/library-asm/mul16.asm
+++ src/arch/zxnext/library-asm/mul16.asm
@@ -5,32 +5,30 @@ __MUL16:	; Mutiplies HL with the last value stored into de stack
 
     PROC
 
-    LOCAL __MUL16LOOP
-    LOCAL __MUL16NOADD
-
     ex de, hl
     pop hl		; Return address
     ex (sp), hl ; CALLEE caller convention
 
 __MUL16_FAST:
-    ld b, 16
-    ld a, h
-    ld c, l
-    ld hl, 0
-
-__MUL16LOOP:
-    add hl, hl  ; hl << 1
-    sla c
-    rla         ; a,c << 1
-    jp nc, __MUL16NOADD
-    add hl, de
-
-__MUL16NOADD:
-    djnz __MUL16LOOP
+    ld a,d                      ; a = xh
+    ld d,h                      ; d = yh
+    ld h,a                      ; h = xh
+    ld c,e                      ; c = xl
+    ld b,l                      ; b = yl
+    mul d,e                     ; yh * yl
+    ex de,hl
+    mul d,e                     ; xh * yl
+    add hl,de                   ; add cross products
+    ld e,c
+    ld d,b
+    mul d,e                     ; yl * xl
+    ld a,l                      ; cross products lsb
+    add a,d                     ; add to msb final
+    ld h,a
+    ld l,e                      ; hl = final
 
     ret	; Result in hl (16 lower bits)
 
     ENDP
 
     pop namespace
-
