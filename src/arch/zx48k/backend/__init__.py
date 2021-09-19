a2fa632d0595838e2b3a44de2b58b1023b0a2a7b
Jose Rodriguez <boriel@gmail.com>
2021-09-20 00:33:08 +0200

    feat: add mul8 for zxnext

--- src/arch/zx48k/backend/__init__.py
+++ src/arch/zx48k/backend/__init__.py
@@ -3,7 +3,10 @@
 from src.api.config import OPTIONS
 from src.arch.z80 import backend
 
-from src.arch.z80.backend import *
+from src.arch.z80.backend import tmp_label, _fpop, HI16, INITS, LO16, LABEL_COUNTER, MEMORY, MEMINITS
+from src.arch.z80.backend import QUADS, REQUIRES, TMP_COUNTER, TMP_STORAGES
+from src.arch.z80.backend import emit, emit_end, emit_start
+
 
 __all__ = [
     "tmp_label",
--- src/arch/zxnext/backend/__init__.py
+++ src/arch/zxnext/backend/__init__.py
@@ -2,8 +2,14 @@
 
 from src.api.config import OPTIONS
 from src.arch.z80 import backend
+from src.arch.z80.backend import ICInfo
 
-from src.arch.z80.backend import *
+from src.arch.zxnext.backend._8bit import _mul8
+
+from src.arch.z80.backend import tmp_label, _fpop, HI16, INITS, LO16, LABEL_COUNTER, MEMORY, MEMINITS
+from src.arch.z80.backend import QUADS, REQUIRES, TMP_COUNTER, TMP_STORAGES
+from src.arch.z80.backend import emit, emit_end, emit_start
+
 
 __all__ = [
     "tmp_label",
@@ -26,6 +32,10 @@ __all__ = [
 
 # ZXNext asm enabled by default for this arch
 OPTIONS.zxnext = True
+
+
+# Override z80 generic implementation with ZX Next ones
+QUADS.update({"muli8": ICInfo(3, _mul8), "mulu8": ICInfo(3, _mul8)})
 
 
 def init():
