4045c474505841f106bdfcfca13182f69cc85eb4
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-07-31 21:03:29 +0200

    ref: move _ltu16 function into a class (Bits16)

--- src/arch/z80/backend/_16bit.py
+++ src/arch/z80/backend/_16bit.py
@@ -482,20 +482,20 @@ class Bits16:
         output.append("push hl")
         return output
 
+    @classmethod
+    def ltu16(cls, ins: Quad) -> list[str]:
+        """Compares & pops top 2 operands out of the stack, and checks
+        if the 1st operand < 2nd operand (top of the stack).
+        Pushes 0 if False, 1 if True.
 
-def _ltu16(ins: Quad) -> list[str]:
-    """Compares & pops top 2 operands out of the stack, and checks
-    if the 1st operand < 2nd operand (top of the stack).
-    Pushes 0 if False, 1 if True.
-
-    16 bit unsigned version
-    """
-    output = Bits16.get_oper(ins[2], ins[3])
-    output.append("or a")
-    output.append("sbc hl, de")
-    output.append("sbc a, a")
-    output.append("push af")
-    return output
+        16 bit unsigned version
+        """
+        output = Bits16.get_oper(ins[2], ins[3])
+        output.append("or a")
+        output.append("sbc hl, de")
+        output.append("sbc a, a")
+        output.append("push af")
+        return output
 
 
 def _lti16(ins: Quad) -> list[str]:
