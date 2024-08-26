ec126e4512ef9a81fdb81ea7a650b15050dfba70
Bartolomé Sánchez Salado <bsanchezsalado@gmail.com>
2024-08-26 23:33:07 +0200

    ref: move _leu16 function into a class (Bits16)

--- src/arch/z80/backend/_16bit.py
+++ src/arch/z80/backend/_16bit.py
@@ -538,21 +538,21 @@ class Bits16:
         output.append("push af")
         return output
 
+    @classmethod
+    def leu16(cls, ins: Quad) -> list[str]:
+        """Compares & pops top 2 operands out of the stack, and checks
+        if the 1st operand <= 2nd operand (top of the stack).
+        Pushes 0 if False, 1 if True.
 
-def _leu16(ins: Quad) -> list[str]:
-    """Compares & pops top 2 operands out of the stack, and checks
-    if the 1st operand <= 2nd operand (top of the stack).
-    Pushes 0 if False, 1 if True.
-
-    16 bit unsigned version
-    """
-    output = Bits16.get_oper(ins[2], ins[3], reversed=True)
-    output.append("or a")
-    output.append("sbc hl, de")  # Carry if A > B
-    output.append("ccf")  # Negates the result => Carry if A <= B
-    output.append("sbc a, a")
-    output.append("push af")
-    return output
+        16 bit unsigned version
+        """
+        output = Bits16.get_oper(ins[2], ins[3], reversed=True)
+        output.append("or a")
+        output.append("sbc hl, de")  # Carry if A > B
+        output.append("ccf")  # Negates the result => Carry if A <= B
+        output.append("sbc a, a")
+        output.append("push af")
+        return output
 
 
 def _lei16(ins: Quad) -> list[str]:
