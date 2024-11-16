4d6adc02362ad9f96cb67fbc1c31c55964004b26
Jose Rodriguez <boriel@gmail.com>
2024-11-17 00:21:37 +0100

    refact: moves SHL I32 and U32 to bitwise folder (runtime)

--- src/arch/z80/backend/runtime/core.py
+++ src/arch/z80/backend/runtime/core.py
@@ -233,7 +233,7 @@ REQUIRED_MODULES = {
     CoreLabels.PSTORE_STR2: "pstorestr2.asm",
     CoreLabels.SHL32: "bitwise/shl32.asm",
     CoreLabels.SHRA32: "bitwise/shra32.asm",
-    CoreLabels.SHRL32: "shrl32.asm",
+    CoreLabels.SHRL32: "bitwise/shrl32.asm",
     CoreLabels.STOREF: "storef.asm",
     CoreLabels.STOP: "error.asm",
     CoreLabels.STORE32: "store32.asm",
