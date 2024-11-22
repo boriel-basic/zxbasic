b7f0d36fe6ffcad6d7dadcfe4053d600213cb7b7
Jose Rodriguez <boriel@gmail.com>
2024-11-23 00:13:34 +0100

    refact: rename decorator to be more PEP8

--- src/api/decorator.py
+++ src/api/decorator.py
@@ -1,7 +1,7 @@
 from collections.abc import Callable
 
 
-class classproperty:
+class ClassProperty:
     """Decorator for class properties.
     Use @classproperty instead of @property to add properties
     to the class object.
@@ -12,3 +12,8 @@ class classproperty:
 
     def __get__(self, owner_self, owner_cls: type):
         return self.fget(owner_cls)
+
+
+def classproperty(fget: Callable[[type], Callable]) -> ClassProperty:
+    """Use this function as the decorator in lowercase to follow Python conventions."""
+    return ClassProperty(fget)
