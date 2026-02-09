82a0f79ae54067fd1bde0e17848b68f85f7943da
Jose Rodriguez <boriel@gmail.com>
2026-02-09 18:45:02 +0100

    feat: add test

---
+++ tests/functional/arch/zx48k/cast_const_to_int.asm
@@ -0,0 +1,212 @@
+	org 32768
+.core.__START_PROGRAM:
+	di
+	push ix
+	push iy
+	exx
+	push hl
+	exx
+	ld (.core.__CALL_BACK__), sp
+	ei
+	jp .core.__MAIN_PROGRAM__
+.core.__CALL_BACK__:
+	DEFW 0
+.core.ZXBASIC_USER_DATA:
+	; Defines USER DATA Length in bytes
+.core.ZXBASIC_USER_DATA_LEN EQU .core.ZXBASIC_USER_DATA_END - .core.ZXBASIC_USER_DATA
+	.core.__LABEL__.ZXBASIC_USER_DATA_LEN EQU .core.ZXBASIC_USER_DATA_LEN
+	.core.__LABEL__.ZXBASIC_USER_DATA EQU .core.ZXBASIC_USER_DATA
+_var_neg:
+	DEFB 00h
+	DEFB 80h
+	DEFB 0FEh
+	DEFB 0FFh
+_var:
+	DEFB 00h
+	DEFB 80h
+	DEFB 01h
+	DEFB 00h
+_var_neg_fl:
+	DEFB 81h
+	DEFB 0C0h
+	DEFB 00h
+	DEFB 00h
+	DEFB 00h
+_r_int:
+	DEFB 00, 00
+_r_long:
+	DEFB 00, 00, 00, 00
+_r_ulong:
+	DEFB 00, 00, 00, 00
+.core.ZXBASIC_USER_DATA_END:
+.core.__MAIN_PROGRAM__:
+	ld hl, (_var_neg)
+	ld de, (_var_neg + 2)
+	ex de, hl
+	ld (_r_int), hl
+	ld hl, 65534
+	ld (_r_int), hl
+	ld hl, (_var_neg)
+	ld de, (_var_neg + 2)
+	ex de, hl
+	ld de, 0
+	ld (_r_long), hl
+	ld (_r_long + 2), de
+	ld hl, (_var_neg)
+	ld de, (_var_neg + 2)
+	ex de, hl
+	ld de, 0
+	ld (_r_ulong), hl
+	ld (_r_ulong + 2), de
+	ld hl, (_var)
+	ld de, (_var + 2)
+	ex de, hl
+	ld (_r_int), hl
+	ld hl, 1
+	ld (_r_int), hl
+	ld hl, (_var)
+	ld de, (_var + 2)
+	ex de, hl
+	ld de, 0
+	ld (_r_long), hl
+	ld (_r_long + 2), de
+	ld hl, (_var)
+	ld de, (_var + 2)
+	ex de, hl
+	ld de, 0
+	ld (_r_ulong), hl
+	ld (_r_ulong + 2), de
+	ld a, (_var_neg_fl)
+	ld de, (_var_neg_fl + 1)
+	ld bc, (_var_neg_fl + 3)
+	call .core.__FTOU32REG
+	ld (_r_int), hl
+	ld hl, 65534
+	ld (_r_int), hl
+	ld a, (_var_neg_fl)
+	ld de, (_var_neg_fl + 1)
+	ld bc, (_var_neg_fl + 3)
+	call .core.__FTOU32REG
+	ld (_r_long), hl
+	ld (_r_long + 2), de
+	ld a, (_var_neg_fl)
+	ld de, (_var_neg_fl + 1)
+	ld bc, (_var_neg_fl + 3)
+	call .core.__FTOU32REG
+	ld (_r_ulong), hl
+	ld (_r_ulong + 2), de
+	ld hl, 0
+	ld b, h
+	ld c, l
+.core.__END_PROGRAM:
+	di
+	ld hl, (.core.__CALL_BACK__)
+	ld sp, hl
+	exx
+	pop hl
+	exx
+	pop iy
+	pop ix
+	ei
+	ret
+	;; --- end of user code ---
+#line 1 "/zxbasic/src/lib/arch/zx48k/runtime/ftou32reg.asm"
+#line 1 "/zxbasic/src/lib/arch/zx48k/runtime/neg32.asm"
+	    push namespace core
+__ABS32:
+	    bit 7, d
+	    ret z
+__NEG32: ; Negates DEHL (Two's complement)
+	    ld a, l
+	    cpl
+	    ld l, a
+	    ld a, h
+	    cpl
+	    ld h, a
+	    ld a, e
+	    cpl
+	    ld e, a
+	    ld a, d
+	    cpl
+	    ld d, a
+	    inc l
+	    ret nz
+	    inc h
+	    ret nz
+	    inc de
+	    ret
+	    pop namespace
+#line 2 "/zxbasic/src/lib/arch/zx48k/runtime/ftou32reg.asm"
+	    push namespace core
+__FTOU32REG:	; Converts a Float to (un)signed 32 bit integer (NOTE: It's ALWAYS 32 bit signed)
+	    ; Input FP number in A EDCB (A exponent, EDCB mantissa)
+    ; Output: DEHL 32 bit number (signed)
+	    PROC
+	    LOCAL __IS_FLOAT
+	    LOCAL __NEGATE
+	    or a
+	    jr nz, __IS_FLOAT
+	    ; Here if it is a ZX ROM Integer
+	    ld h, c
+	    ld l, d
+	    ld d, e
+	    ret
+__IS_FLOAT:  ; Jumps here if it is a true floating point number
+	    ld h, e
+	    push hl  ; Stores it for later (Contains Sign in H)
+	    push de
+	    push bc
+	    exx
+	    pop de   ; Loads mantissa into C'B' E'D'
+	    pop bc	 ;
+	    set 7, c ; Highest mantissa bit is always 1
+	    exx
+	    ld hl, 0 ; DEHL = 0
+	    ld d, h
+	    ld e, l
+	    ;ld a, c  ; Get exponent
+	    sub 128  ; Exponent -= 128
+	    jr z, __FTOU32REG_END	; If it was <= 128, we are done (Integers must be > 128)
+	    jr c, __FTOU32REG_END	; It was decimal (0.xxx). We are done (return 0)
+	    ld b, a  ; Loop counter = exponent - 128
+__FTOU32REG_LOOP:
+	    exx 	 ; Shift C'B' E'D' << 1, output bit stays in Carry
+	    sla d
+	    rl e
+	    rl b
+	    rl c
+	    exx		 ; Shift DEHL << 1, inserting the carry on the right
+	    rl l
+	    rl h
+	    rl e
+	    rl d
+	    djnz __FTOU32REG_LOOP
+__FTOU32REG_END:
+	    pop af   ; Take the sign bit
+	    or a	 ; Sets SGN bit to 1 if negative
+	    jp m, __NEGATE ; Negates DEHL
+	    ret
+__NEGATE:
+	    exx
+	    ld a, d
+	    or e
+	    or b
+	    or c
+	    exx
+	    jr z, __END
+	    inc l
+	    jr nz, __END
+	    inc h
+	    jr nz, __END
+	    inc de
+	LOCAL __END
+__END:
+	    jp __NEG32
+	    ENDP
+__FTOU8:	; Converts float in C ED LH to Unsigned byte in A
+	    call __FTOU32REG
+	    ld a, l
+	    ret
+	    pop namespace
+#line 72 "arch/zx48k/cast_const_to_int.bas"
+	END
